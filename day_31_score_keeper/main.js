a789f93d68086f7a1c329ff1ccbb7be31a123a09
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-09 14:31:19 -0500

    Add Ping Pong Scorer functional version

--- day_31_score_keeper/main.js
+++ day_31_score_keeper/main.js
@@ -1,6 +1,6 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
-const reset = document.querySelector('#resetButton');
+const resetButton = document.querySelector('#resetButton');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 const winningScoreSelect = document.querySelector('#playTo');
@@ -15,6 +15,10 @@ p1Button.addEventListener('click', function () {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -25,6 +29,11 @@ p2Button.addEventListener('click', function () {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+
         }
         p2Display.textContent = p2Score;
     }
@@ -44,4 +53,8 @@ function reset() {
     p2Score = 0;
     p1Display.textContent = '0';
     p2Display.textContent = '0';
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
