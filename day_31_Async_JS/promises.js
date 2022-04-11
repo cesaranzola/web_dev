3fea0f84d95f6d9549aa4c470d8dc5645e0cd9fd
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-10 22:15:28 -0500

    Promises then and catch methods explanation

--- day_31_Async_JS/promises.js
+++ day_31_Async_JS/promises.js
@@ -1,4 +1,4 @@
-// THE PROMISE VERSION 
+//=================Peomise Fake Request Function========================
 const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
         const delay = Math.floor(Math.random() * (4500)) + 500;
@@ -12,3 +12,25 @@ const fakeRequestPromise = (url) => {
     })
 }
 
+
+fakeRequestPromise('yelp.com/api/coffee')
+    .then(() => {
+        console.log('It worked! (page 1)');
+        fakeRequestPromise('yelp.com/api/coffee/pag2')
+            .then(() => {
+                console.log('It worked! j(page 2)');
+                fakeRequestPromise('yelp.com/api/coffee/pag3')
+                    .then(() => {
+                        console.log('It worked! (page 3)');
+                    })
+                    .catch(() => {
+                        console.log('Promise rejected!(page 3)');
+                    })
+            })
+            .catch(() => {
+                console.log('Promise rejected!(page 2)');
+            })
+    })
+    .catch(() => {
+        console.log('Promise rejected! (page 1)');
+    })
