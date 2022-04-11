3e17d95022e9d1406f43d68dfeae60b1b78f7225
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-10 20:54:44 -0500

    Add final explanation for Callback hell

--- day_31_Async_JS/fake_request_promises.js
+++ day_31_Async_JS/fake_request_promises.js
@@ -1,24 +1,35 @@
-// THE CALLBACK VERSION
 const fakeRequestCallback = (url, success, failure) => {
-    const delay = Math.floor(Math.random() * 4500) + 500;
+    const delay = Math.floor(Math.random() * 4000) + 500;
     setTimeout(() => {
         if (delay > 4000) {
-            failure('Connection Timeout :(')
+            failure('Connection timeout!');
         } else {
-            success(`Here is your fake data from ${url}`)
+            success(`Here is your fake data from ${url}`);
         }
     }, delay)
 }
-// THE PROMISE VERSION 
-const fakeRequestPromise = (url) => {
-    return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
-        setTimeout(() => {
-            if (delay > 4000) {
-                reject('Connection Timeout :(')
-            } else {
-                resolve(`Here is your fake data from ${url}`)
-            }
-        }, delay)
-    })
-}
+
+
+fakeRequestCallback('books.com/pag1', function (response) {
+    console.log('It worked!');
+    console.log(response);
+    fakeRequestCallback('books.com/pag2',
+        function (response) {
+            console.log('It worked. (2nd request.');
+            console.log(response);
+            fakeRequestCallback('books.com/pag3',
+                function (response) {
+                    console.log('It worked. (3rd request.');
+                    console.log(response);
+                },
+                function (err) {
+                    console.log('Error! (3rd request)', err)
+                })
+        },
+        function (err) {
+            console.log('Error! (2nd request)', err)
+        }
+    )
+}, function (err) {
+    console.log('Error!', err)
+})
