412e2a5cbbd83a5113840266b778202879710da4
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-11 12:32:14 -0500

    Add the Color change function

--- day_32_Async_JS/async_functions.js
+++ day_32_Async_JS/async_functions.js
@@ -48,18 +48,41 @@
 //     })
 
 
-const login = async (username, password) => {
-    if (!username || !password) throw 'Missing credentials'
-    if (password === 'rockingInTheFreeWorld') return 'Welcome again!'
-    throw 'Invalid password'
+// const login = async (username, password) => {
+//     if (!username || !password) throw 'Missing credentials'
+//     if (password === 'rockingInTheFreeWorld') return 'Welcome again!'
+//     throw 'Invalid password'
+// }
+
+// login('pepo', 'rockingInTheFreeWorld')
+//     .then((msg) => {
+//         console.log('Logged In');
+//         console.log(msg);
+//     })
+//     .catch((err) => {
+//         console.log('Error 650');
+//         console.log(err);
+//     })
+
+//The await keyword
+//We can only use the await keyword inside of functions declared with async.
+//await will pause the execution of the function, waiting for promise to be resolved
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
 }
 
-login('pepo', 'rockingInTheFreeWorld')
-    .then((msg) => {
-        console.log('Logged In');
-        console.log(msg);
-    })
-    .catch((err) => {
-        console.log('Error 650');
-        console.log(err);
-    })
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('blue', 1000))
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('indigo', 1000))
+    .then(() => delayedColorChange('cyan', 1000))
+    .then(() => delayedColorChange('violet', 1000))
+    .then(() => delayedColorChange('paleblue', 1000))
+
+
