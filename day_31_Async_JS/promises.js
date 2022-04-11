f212cd541c4559298293875f31ba369d632bb085
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-10 20:56:17 -0500

    Add boilerplate files for Promises

---
+++ day_31_Async_JS/promises.js
@@ -0,0 +1,14 @@
+// THE PROMISE VERSION 
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
