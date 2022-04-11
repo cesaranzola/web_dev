52aa6247b3eac1521ba3331f57240b660d77c44c
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-11 17:38:20 -0500

    String Query, APIs, Requests and status codes

---
+++ day_32_AJAX/http_postman.js
@@ -0,0 +1,46 @@
+//=================GET REQUEST===================
+// GET Request - retrieving or getting information
+// POST Request - use for sending data, that we intend to impact somehow the serverside, like saving something to a database or updating a server.
+// The server determines which request are valid or invalid/ If you try to use a Get request as a Post request, the server will complaint and the request won't be executed.
+
+
+//STATUS CODES
+// You can look up the most common status codes in MDN
+// HTTP response status codes indicate whether a specific HTTP request has been successfully completed. Responses are grouped in five classes:
+// Informational responses (100–199)
+// Successful responses (200–299)
+// Redirection messages (300–399)
+// Client error responses (400–499)
+// Server error responses (500–599)
+
+
+//Headers
+//They are kind of the metadata for the response and the request.
+// You can see the type of data the server is responding to your request with.
+
+//Lingo 
+//API endpoint - Simply put, an endpoint is one end of a communication channel. 
+//When an API interacts with another system, the touchpoints of this communication are considered endpoints. For APIs, an endpoint can include a URL of a server or service.
+
+// API Ticker - url to access the API endpoint
+
+//API -Everytime you see a colon in an API documentation, it means it's a variable, and you have to provide the information after the colon.
+
+
+
+//Query string 
+//What is a query? A query is a question or a request for information expressed in a formal manner.
+// A querystring is a set of characters input to a computer or Web browser and sent to a query program to recover specific information from a database . 
+// On the Internet, a querystring (also called an HTTP querystring) is part of the set of characters automatically input in the address bar of a dynamic Web site 
+// when a user makes a request for information according to certain criteria.
+
+//Quary string summary - a key value pair we pass into any URL to request information
+// the Quer string is additional info, you're not chaging the URL in itself
+
+// Below you can see an URL tha has at the end ?q=money > this gets added when you perform for example a search in website.
+// the q can be change depending on what you set, q happens to be the standard for multiple sites.
+// https://www.reddit.com/search/?q=money
+
+
+
+//APIS - some APIS will ask you to set a header on your API ticker
