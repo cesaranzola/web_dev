93ddba68d2e01a1e5e0c55a858b627cdee9d4095
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-11 09:39:15 -0500

    Refactor code for Then and catch syntax promise fix

--- day_31_Async_JS/the_real_promises.js
+++ day_31_Async_JS/the_real_promises.js
@@ -1,37 +1,3 @@
-// const fakeRequestPromise = (url) => {
-//     return new Promise((data, reject) => {
-//         const delay = Math.floor(Math.random() * (4000)) + 500;
-//         setTimeout(() => {
-//             if (delay > 4000) {
-//                 reject('Connection timeout!');
-//             } else {
-//                 data(`Here is your fake data from ${url}.`);
-//             }
-//         }, delay)
-//     })
-// }
-
-// fakeRequestPromise('yelp.com/api/coffee/page1')
-//     .then((data) => {
-//         console.log('It worked. (page 1)')
-//         console.log(data);
-//         return fakeRequestPromise('yelp.com/api/coffee/page2')
-//     })
-//     .then((data) => {
-//         console.log('It worked. (page 2)')
-//         console.log(data);
-//         return fakeRequestPromise('yelp.com/api/coffee/page3')
-//     })
-//     .then((data) => {
-//         console.log('It woarked. (page 3)');
-//         console.log(data);
-//         return fakeRequestPromise('yelp.com.api/coffee/page3')
-//     })
-//     .catch((reject) => {
-//         console.log('Error. The request failed!')
-//     })
-
-
 const fakeRequestPromise = (url) => {
     return new Promise((data, reject) => {
         const delay = Math.floor(Math.random() * (4000)) + 500;
