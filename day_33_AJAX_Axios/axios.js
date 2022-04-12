d712ae82586812e158f4cb823432c8cc03f29b7a
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-12 12:00:26 -0500

    Refactor the getDadJoke Async function

--- day_33_AJAX_Axios/axios.js
+++ day_33_AJAX_Axios/axios.js
@@ -9,12 +9,77 @@
 //     })
 
 //============Async Function using Axios==================
-const randomCatFact = async () => {
+// const randomCatFact = async () => {
+//     try {
+//         const res = await axios.get('https://catfact.ninja/fact');
+//         console.log(res.data.fact);
+//     } catch (e) {
+//         console.log('Error. The request denied.', e);
+//     }
+// }
+// randomCatFact();
+
+//================Axios with passing Headers parameters===================
+
+//The upsides of working with an API
+//Making a simple request with an API, and then doing something to update
+//what the user sees or change their experience, where the page
+//never actually refreshed, we never navigated anywhere else. 
+
+
+// Select elements to interact with the DOM
+// const jokes = document.querySelector('#jokes');
+// const button = document.querySelector('button'); //Because it's a small example we assume that it's the only button present on the page. For other cases, you need to change this.
+
+
+// // Async function to add each time a li element (joke in this case) to the DOM
+// const addNewJoke = async () => {
+//     const jokeText = await getDadJoke();
+//     console.log(jokeText);
+//     const newLi = document.createElement('li');
+//     newLi.append(jokeText);
+//     jokes.append(newLi);
+// }
+
+// // Async function to extract the joke as a text
+// const getDadJoke = async () => {
+//     try {
+//         const config = { headers: { Accept: 'application/json' } };
+//         const res = await axios.get('https://icanhaddzdadjoke.com/', config);
+//         return res.data.joke
+//     } catch (e) {
+//         return 'No Dad jokes available at the moment.'
+//     }
+// }
+
+// button.addEventListener('click', addNewJoke)
+
+
+
+//Select the DOM elements
+const button = document.querySelector('button');
+const ul = document.querySelector('#jokes');
+
+
+// Async Function to grab text from API
+const getDadJoke = async () => {
     try {
-        const res = await axios.get('https://catfact.ninja/fact');
-        console.log(res.data.fact);
+        const header = { headers: { Accept: 'application/json' } };
+        const getText = await axios.get('https://icanhazdadjoke.com/', header);
+        return getText.data.joke
     } catch (e) {
-        console.log('Error. The request denied.', e);
+        return 'No Dad jokes available at the moment.'
     }
 }
-randomCatFact();
+
+//Async Function to a new li element to ul by means of the DOM
+const addNewJoke = async () => {
+    const newLi = document.createElement('li');
+    const getJoke = await getDadJoke();
+    newLi.append(getJoke);
+    ul.append(newLi);
+}
+
+//Event listener - Adds a new joke as a new li to the DOM
+button.addEventListener('click', addNewJoke)
+
