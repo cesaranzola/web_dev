fa0df3daf4a8820308e9060c3cf3e832c812c1c3
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-12 10:22:44 -0500

    Async function and await keyword using Axios

--- day_33_AJAX_Axios/axios.js
+++ day_33_AJAX_Axios/axios.js
@@ -1,13 +1,20 @@
 //===================AXIOS library=====================
 // When the promise is resolved, you immediately get the data, you don't have to parse it like we have to do it with Fetch
-axios.get('https://catfact.ninja/fact')
-    .then(res => {
-        console.log(res.data.fact)
-    })
-    .catch(e => {
-        console.log('Error. Data request denied.', e)
-    })
+// axios.get('https://catfact.ninja/fact')
+//     .then(res => {
+//         console.log(res.data.fact)
+//     })
+//     .catch(e => {
+//         console.log('Error. Data request denied.', e)
+//     })
 
-
-
-
+//============Async Function using Axios==================
+const randomCatFact = async () => {
+    try {
+        const res = await axios.get('https://catfact.ninja/fact');
+        console.log(res.data.fact);
+    } catch (e) {
+        console.log('Error. The request denied.', e);
+    }
+}
+randomCatFact();
