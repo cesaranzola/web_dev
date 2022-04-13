556267217ea74645f570abfccbff442de85760ca
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-13 12:09:29 -0500

    Third Example

--- day_35_OOO_prototypes_class/js_classes.js
+++ day_35_OOO_prototypes_class/js_classes.js
@@ -13,19 +13,49 @@
 
 // const lieblingsFarbe = new Color(244, 55, 62);
 
-class Color {
-    constructor(r, g, b, name) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.name = name;
-    }
-    //How to define a new method using classes and the constructor
-    //This method will be save in the prototype of the object, not each instance. 
-    greet() {
-        return `I'm Mr. ${this.name}, and I'm gonna make you an offer, you can refuse...`
-    }
-}
 
-const lieblingsFarbe = new Color(255, 67, 89, 'Tomato');
-const lieblingsFarbeZwei = new Color(255, 255, 255, 'White');
+//Second Class Example
+// class Color {
+//     constructor(r, g, b, name) {
+//         this.r = r;
+//         this.g = g;
+//         this.b = b;
+//         this.name = name;
+//     }
+//     //How to define a new method using classes and the constructor
+//     //This method will be save in the prototype of the object, not each instance. 
+//     greet() {
+//         return `I'm Mr. ${this.name}, and I'm gonna make you an offer, you can refuse...`
+//     }
+// }
+
+// const lieblingsFarbe = new Color(255, 67, 89, 'Tomato');
+// const lieblingsFarbeZwei = new Color(255, 255, 255, 'White');
+
+
+//Third Class example
+// class Color {
+//     constructor(r, g, b, name) {
+//         this.r = r;
+//         this.g = g;
+//         this.b = b;
+//         //Anytime you're inside of the class and have use the new keyword to create the object
+//         //this refers to instance of the class, the individual color object, not the prototype, not the function, not the window, 
+//     }
+//     rgb() {
+//         const { r, g, b } = this; // You still have to desctructure the object properties into different variables
+//         return `rgb(${r}, ${g}, ${b})`
+//     }
+//     hex() {
+//         const { r, g, b } = this;
+//         return (
+//             '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+//         );
+//     }
+// }
+
+// const lieblingsFarbe = new Color(21, 135, 67, 'Green');
+// const lieblingsFarbeZwei = new Color(21, 27, 135, 'Blue');
+
+
+//Example Fourth - Calling a method inside the class and constructor
