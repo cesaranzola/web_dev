9616de35f2e7d188c604934e25e9acad5b6c293d
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-13 12:41:50 -0500

    Example on how to call previously defined method inside the class, within another method

--- day_35_OOO_prototypes_class/js_classes.js
+++ day_35_OOO_prototypes_class/js_classes.js
@@ -59,3 +59,38 @@
 
 
 //Example Fourth - Calling a method inside the class and constructor
+// class Color {
+//     constructor(r, g, b, name) {
+//         //This functio will run everytime you instantiate a new instance of the class
+//         //The properties are added to the newly created object
+//         this.r = r;
+//         this.g = g;
+//         this.b = b;
+//         this.name = name;
+//     }
+
+//     //All these methods are added to the prototype of the Color class, not to every instance that you instantiate. This is very important to understand.
+
+//     innerRGB() {
+//         const { r, g, b } = this;
+//         return `${r}, ${g}, ${b}`
+//     }
+//     //How to call methods from within another method on the class
+//     rgb() {
+//         return `rgb(${this.innerRGB()})`;
+//     }
+//     rgba(a = 1.0) {
+//         return `rgba(${this.innerRGB()}, ${a})`;
+//     }
+//     hex() {
+//         const { r, g, b } = this;
+//         return (
+//             '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+//         );
+//     }
+// }
+
+// const weiß = new Color(255, 255, 255, 'white');
+// const rot = new Color(255, 67, 89, 'red'); 
+
+//Example 5 - 
