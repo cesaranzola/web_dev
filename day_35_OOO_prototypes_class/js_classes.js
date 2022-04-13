8e6f0552afffdc202481d93d8b4f1f38072be396
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-13 14:27:21 -0500

    Add final Class with multiple methods and instantiate objects with the new keyword

--- day_35_OOO_prototypes_class/js_classes.js
+++ day_35_OOO_prototypes_class/js_classes.js
@@ -93,4 +93,97 @@
 // const weiß = new Color(255, 255, 255, 'white');
 // const rot = new Color(255, 67, 89, 'red'); 
 
-//Example 5 - 
+//Example 5 - HSL (Hue, Saturation, Lightness)
+
+class Color {
+    constructor(r, g, b, name) {
+        //Addding values to the instantiate instance object 
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+        //Using a method define within the class
+        this.calcHSL(); // Immediately apply the calcHSL() method to the r,g,b values being passed.
+    }
+
+    //Methods
+    innerRGB() {
+        const { r, g, b } = this;
+        return `${r}, ${g}, ${b}`
+    }
+    rgb() {
+        return `rgb(${this.innerRGB()})`;
+    }
+    rgba(a = 1.0) {
+        return `rgba(${this.innerRGB()}, ${a})`;
+    }
+    hex() {
+        const { r, g, b } = this;
+        return (
+            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+        );
+    }
+    hsl() {
+        const { h, s, l } = this;
+        return `hsl(${h}, ${s}%, ${l}%)`;
+    }
+    opposite() {
+        const { h, s, l } = this;
+        const newHue = (h + 180) % 360;
+        return `hsl(${newHue}, ${s}%, ${l}%)`;
+    }
+    fullySaturated() {
+        const { h, l } = this;
+        return `hsl(${h}, 100%, ${l}%)`;
+    }
+    calcHSL() {
+        let { r, g, b } = this;
+        // Make r, g, and b fractions of 1
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        // Find greatest and smallest channel values
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            // Red is max
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            // Green is max
+            h = (b - r) / delta + 2;
+        else
+            // Blue is max
+            h = (r - g) / delta + 4;
+
+        h = Math.round(h * 60);
+
+        // Make negative hues positive behind 360°
+        if (h < 0) h += 360;
+        // Calculate lightness
+        l = (cmax + cmin) / 2;
+
+        // Calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        // Multiply l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
+
+    }
+}
+
+//Instantiate instances of class Color using the new keyword 
+const weiß = new Color(255, 255, 255, 'white');
+const rot = new Color(255, 67, 89, 'red');
+const orange = new Color(220, 126, 32, 'carrot');
+
+
