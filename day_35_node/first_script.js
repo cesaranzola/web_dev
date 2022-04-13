cc27abdcb7261ced6b9aa43e0128a592c914d06c
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-13 17:52:07 -0500

    Add first node script

---
+++ day_35_node/first_script.js
@@ -0,0 +1,10 @@
+
+for (let i = 0; i < 10; i++) {
+    console.log('Hello from First Script');
+}
+
+//Process is an object available in the global scope.
+// process.version
+// process.release
+// process.cwd > current working directory
+// 
