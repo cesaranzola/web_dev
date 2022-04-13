0b7a87675bcf16b5a82695fc9432ec9aaa027c94
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-13 11:43:55 -0500

    Second Class Example

---
+++ day_35_OOO_prototypes_class/js_classes.js
@@ -0,0 +1,31 @@
+//====================CLASSES===============
+//Use an uppercase letter to indicate it's a class or constructor function
+
+// class Color {
+//     //It's a function that will execute immediately whenever a new color (object) is created.
+//     //Think of constructor as the function constructor you create before (Refer to the previous subject in dir)
+//     constructor(r, g, b) {
+//         console.log('Inside the constructor'); //This is to show that the constructor runs immediately, wihtout having to call it.
+//         //Whenever you instantiate a new instance of the Color object, it will run immediately
+//         console.log(r, g, b);
+//     }
+// }
+
+// const lieblingsFarbe = new Color(244, 55, 62);
+
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    //How to define a new method using classes and the constructor
+    //This method will be save in the prototype of the object, not each instance. 
+    greet() {
+        return `I'm Mr. ${this.name}, and I'm gonna make you an offer, you can refuse...`
+    }
+}
+
+const lieblingsFarbe = new Color(255, 67, 89, 'Tomato');
+const lieblingsFarbeZwei = new Color(255, 255, 255, 'White');
