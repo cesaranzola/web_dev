ddc13a3d9796307131c6742f57083a44620bda2f
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-13 21:55:06 -0500

    Add args script to test

---
+++ day_35_node/args.js
@@ -0,0 +1,2 @@
+console.log('Hello from args file');
+console.log(process.argv); // An array that contains a string
--- day_35_node/first_script.js
+++ day_35_node/first_script.js
@@ -8,3 +8,5 @@ for (let i = 0; i < 10; i++) {
 // process.release
 // process.cwd > current working directory
 // 
+
+//
