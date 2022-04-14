3bd70455485e97d66cd3a894e01e771bbb04509a
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-13 23:44:52 -0500

    Refactor boilerplate script

--- day_35_node/boilerplate.js
+++ day_35_node/boilerplate.js
@@ -1,7 +1,15 @@
 import { mkdirSync, writeFileSync } from "fs";
 const folderName = process.argv[2] || 'Project'
 
-mkdirSync(folderName);
-writeFileSync(`${folderName}/index.html`)
-writeFileSync(`${folderName}/app.js`)
-writeFileSync(`${folderName}/styles.css`)
+try {
+    mkdirSync(folderName);
+    writeFileSync(`${folderName}/index.html`, '')
+    writeFileSync(`${folderName}/app.js`, '')
+    writeFileSync(`${folderName}/styles.css`, '')
+} catch (e) {
+    console.log('Something went wrong...');
+    console.log(e);
+}
+
+
+//The Folder and files are created in the location you run the script, not in the script location
