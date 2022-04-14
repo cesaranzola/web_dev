c0aaeabd51be3ccc5326f6082a823ad7404e2b65
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-14 13:42:17 -0500

    Add joke and colors packages locally

--- day_36_npm_universe/Joke/index.js
+++ day_36_npm_universe/Joke/index.js
@@ -4,3 +4,7 @@ const colors = require('colors');
 giveMeAJoke.getRandomCNJoke(function (joke) {
     console.log(joke.rainbow);
 });
+
+// npm i {packageName} -g
+// For the most part you want to install your packages locally 
+//npm link {packageName} > To access the global package, when they aren't installed in our local directory
