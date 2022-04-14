0d2e0157b34de8fa9e5fa204536da3cf72f393b9
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-14 10:05:31 -0500

    Add different ways in which you can export modules

--- day_36_npm_universe/math.js
+++ day_36_npm_universe/math.js
@@ -1,3 +1,35 @@
+// const add = (x, y) => x + y;
+// const PI = 3.14159;
+// const square = x => x * x;
+
+//Add module exports so you can export access the declare module exports in other files
+//Module.exports is an object, and we're addding the following properties to that object
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+
+//Different way of defining the object to access it from other files
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+
+// module.exports = math;
+
+
+//Third way is to add them directly
+// module.exports.add = (x, y) => x + y;
+// module.exports.PI = 3.14159;
+// module.exports.square = x => x * x;
+
+
+//Shorter way of adding properties to the object
 const add = (x, y) => x + y;
 const PI = 3.14159;
 const square = x => x * x;
+
+exports.add = add;
+exports.square = square;
+exports.PI = PI;
--- day_36_npm_universe/require_script.js
+++ day_36_npm_universe/require_script.js
@@ -1,2 +1,11 @@
-const math = require('./math'); // When you're referencing a file rather than a module, you have to add the ./ to make it clear you're referencing something in the current diretory
-console.log(math);
+// const math = require('./math'); // When you're referencing a file rather than a module, you have to add the ./ to make it clear you're referencing something in the current diretory
+// //Now we have access to the math object and its functionalities
+// console.log(math.PI);
+// console.log(math.square(4));
+
+
+//You can destructure the object to define which properties do you want access to
+// The add property currently present in the math object is being ignored in the destructuring process.
+const { PI, square } = require('./math'); // If you dereference the object, you can ommit the name of the object when calling its methods.
+console.log(PI);
+console.log(square(4));
