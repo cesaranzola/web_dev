73896e4a948880207f8c96f382bd334a17bc8b7f
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-13 22:12:37 -0500

    Add process.argv method

--- day_35_node/args.js
+++ day_35_node/args.js
@@ -1,2 +1,5 @@
 console.log('Hello from args file');
+// The process.argv property returns an array containing the command-line arguments passed when the Node.js process was launched
 console.log(process.argv); // An array that contains a string
+//You can pass arguments to a script
+// The first arguments will be the executable path and the script's path
