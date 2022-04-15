ad9457ce6f51236e28e22bc260724626eb0481f8
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-15 17:27:28 -0500

    Add boilerplate folders and files for reddit app

--- day_37_dynamic_HTML_templating/EJS_HTML_app/index.js
+++ day_37_dynamic_HTML_templating/EJS_HTML_app/index.js
@@ -26,34 +26,71 @@
 
 
 //Clean Code 
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '/views'));
+
+// app.get('/', (req, res) => {
+//     res.render('home');
+// })
+
+// app.get('/rand', (req, res) => {
+//     const num = Math.floor(Math.random() * 10) + 1;
+//     res.render('random', { num: num });
+// })
+
+// app.get('/r/:subreddit', (req, res) => {
+//     const { subreddit } = req.params;
+//     res.render('subreddit', { subreddit })
+// })
+
+// app.get('/movies', (req, res) => {
+//     const movies = [
+//         'The Godfather', 'Crossroads', 'The Doors', 'Monty Python'
+//     ]
+//     res.render('movies', { movies });
+// })
+
+// app.listen(3000, () => {
+//     console.log('Listening to the port: 3000..');
+// })
+
+
+
+
 const express = require('express');
 const app = express();
 const path = require('path');
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/rand', (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { num: num });
-})
-
-app.get('/r/:subreddit', (req, res) => {
-    const { subreddit } = req.params;
-    res.render('subreddit', { subreddit })
-})
-
 app.get('/movies', (req, res) => {
     const movies = [
-        'The Godfather', 'Crossroads', 'The Doors', 'Monty Python'
+        'Romma Cità Aperta', 'Stromboli', 'Europa', 'Paisan'
     ]
     res.render('movies', { movies });
 })
 
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit });
+})
+
+app.get('/random', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { num })
+})
+
 app.listen(3000, () => {
-    console.log('Listening to the port: 3000..');
+    console.log('Listening to the port: 3000...');
 })
