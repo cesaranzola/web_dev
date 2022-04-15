6034cd83e75bea5d31c3240b2796f4f01aa68b14
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-15 14:51:18 -0500

    Add Subreddit view and connect objects to embedd them using EJS

--- day_37_dynamic_HTML_templating/EJS_HTML_app/index.js
+++ day_37_dynamic_HTML_templating/EJS_HTML_app/index.js
@@ -31,10 +31,20 @@ const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
-app.set('views', paht.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { rand: num }); //Creates an object and assigns the num value, you can then access rand in the EJS files of your app.
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit })
 })
 
 
