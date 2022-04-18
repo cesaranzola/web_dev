05fed5e354cb5c1e36f9c5fb58e136a787aa8062
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-17 23:09:21 -0500

    Add delete view

--- day_39_RESTful/RESTDemo/index.js
+++ day_39_RESTful/RESTDemo/index.js
@@ -1,44 +1,45 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const methodOverRide = require("method-override");
 
 // !app.use runs a function on every single request (post, get) it doesn't matter what the path name is
 // Use this middleware to parse the body as urlencoded data
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
-
+app.use(methodOverRide("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-const comments = [
+let comments = [
   {
-    id: 1,
+    id: uuidv4(),
     username: "Todd",
     comment: "If you fuck with the bull, you get the horn!",
   },
   {
-    id: 2,
+    id: uuidv4(),
     username: "Peppo",
     comment: "If you fuck with the bull, you get the horn!",
   },
   {
-    id: 3,
+    id: uuidv4(),
     username: "Claire",
     comment: "If you fuck with the bull, you get the horn!",
   },
   {
-    id: 4,
+    id: uuidv4(),
     username: "Natalie",
     comment: "If you fuck with the bull, you get the horn!",
   },
   {
-    id: 5,
+    id: uuidv4(),
     username: "Brett",
     comment: "If you fuck with the bull, you get the horn!",
   },
   {
-    id: 6,
+    id: uuidv4(),
     username: "Daniel",
     comment: "If you fuck with the bull, you get the horn!",
   },
@@ -56,29 +57,37 @@ app.get("/comments/new", (req, res) => {
 //Route for creating a new comment - post comment
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body;
-  comments.push({ username, comment });
+  comments.push({ username, comment, id: uuidv4() });
   res.redirect("/comments");
 });
 
 //Route to get by id
 app.get("/comments/:id", (req, res) => {
   const { id } = req.params;
-  const comment = comments.find((c) => c.id === parseInt(id));
+  const comment = comments.find((c) => c.id === id);
   res.render("comments/show", { comment });
 });
 
-//Route
-//Remember that you can access the requested data, req is an object that Express creates for us
-//You can access the query of that data through req.query
-//If you want to grab the data in the case of a post request, you can access it via req.body (before you have to define the type in which you want that data to be processed)
-app.get("/tacos", (req, res) => {
-  res.send("GET /tacos response");
+//Patch method - to update a part of a resource ( Use put if you want ot update the entire thing. Read the docs)
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect("/comments");
 });
 
-app.post("/tacos", (req, res) => {
-  const { meat, qty } = req.body;
-  console.log(req.body);
-  res.send(`Enjoy your meal, here are your ${qty} ${meat} tacos!`);
+//Route for faking the edit (due to Html limitation to only two methods > get or post)
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
+});
+
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((c) => c.id !== id);
+  res.redirect("/comments");
 });
 
 app.listen(3000, () => {
