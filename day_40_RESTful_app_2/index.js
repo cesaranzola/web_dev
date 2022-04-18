5db03381804ea69206f8e9aaba1520613fe35039
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-18 10:43:04 -0500

    Create Express app & added the require modules

---
+++ day_40_RESTful_app_2/index.js
@@ -0,0 +1,6 @@
+//===================Require======================
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverRide = require('method-override');
+const { v4: uuidv4 } = require('uuid');
