d8faead735db2edb5f4b0a1ff47092c31f95a8b3
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-18 10:58:36 -0500

    Add view engine & and absolut path for views

--- day_40_RESTful_app_2/index.js
+++ day_40_RESTful_app_2/index.js
@@ -9,3 +9,14 @@ const { v4: uuidv4 } = require('uuid');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverRide(require('_method')));
+
+//================View Engine====================
+app.set('view engine', 'ejs');
+app.set('views', path.join(_dirname, '/views'));
+
+//===================Data========================
+const comments = [
+    {
+        id: uuidv4();
+    }
+]
