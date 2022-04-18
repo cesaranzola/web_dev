147fa80da36efe92ec07ec69cdefccf399eb70bf
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-18 10:54:24 -0500

    Add Data parser through use() & Method override

--- day_40_RESTful_app_2/index.js
+++ day_40_RESTful_app_2/index.js
@@ -4,3 +4,8 @@ const app = express();
 const path = require('path');
 const methodOverRide = require('method-override');
 const { v4: uuidv4 } = require('uuid');
+
+//==================Use==========================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverRide(require('_method')));
