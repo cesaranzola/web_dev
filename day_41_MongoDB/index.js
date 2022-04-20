7dc59cd67834ac6b676bfc44b3e449dc5c348a25
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-19 22:58:00 -0500

    Create boilerplate for Mongoose Basics

---
+++ day_41_MongoDB/index.js
@@ -0,0 +1,32 @@
+db.newDogs.insert([
+  {
+    name: 'Pepo',
+    breed: 'Golden',
+    age: 10,
+    personality: { catFriendly: false, childFriendly: true },
+  },
+  {
+    name: 'Rocky',
+    breed: 'Golden',
+    age: 6,
+    personality: { catFriendly: true, childFriendly: false },
+  },
+  {
+    name: 'Daniel',
+    breed: 'Golden',
+    age: 8,
+    personality: { catFriendly: true, childFriendly: true },
+  },
+  {
+    name: 'Blue',
+    breed: 'Golden',
+    age: 5,
+    personality: { catFriendly: false, childFriendly: false },
+  },
+  {
+    name: 'Fuchtbar',
+    breed: 'Golden',
+    age: 7,
+    personality: { catFriendly: true, childFriendly: true },
+  },
+]);
