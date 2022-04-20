81874a72d8515744b64294b4c2c68443d21ed53f
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-20 11:35:37 -0500

    Add new instance called Bike and update product Schema

--- day_41_Mongoose/product.js
+++ day_41_Mongoose/product.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose', {
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose
-    .connect('mongodb://localhost:27017/movieApp')
+    .connect('mongodb://localhost:27017/showApp')
     .then(() => {
       console.log('Connection open...');
     })
@@ -23,11 +23,48 @@ const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    maxlength: 20,
   },
   price: {
+    //This isn't requiring, but you have to pass in a value that can be transform into an int > '59' > 59
     type: Number,
+    required: true,
+    min: 0,
+  },
+  onSale: {
+    type: Boolean,
+    default: false,
+  },
+  categories: [String], // It one of the values included in the array of string, isn't a string, it will try to cast the value into a string > 99 > '99'
+  qty: {
+    online: {
+      type: Number,
+      default: 0,
+    },
+    inStore: {
+      type: Number,
+      default: 0,
+    },
   },
 });
 
-//==================Model======================
+//==================Model > (collection)======================
 const Product = mongoose.model('Product', productSchema);
+
+//===================New Instance===============
+const bike = new Product({
+  name: 'gloves H',
+  price: 23,
+  onSale: true,
+  categories: ['Cycling', 'Safety', 'Outdoors'],
+});
+bike
+  .save()
+  .then((data) => {
+    console.log('It worked');
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log('Error!');
+    console.log(err);
+  });
