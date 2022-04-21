d73982e8f24e487430eb37db1629a012b87b95c9
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-20 23:34:06 -0500

    Refactor new view

--- day_42_mongoose_Express/index.js
+++ day_42_mongoose_Express/index.js
@@ -10,25 +10,27 @@ const Product = require('./models/product');
 //Let's add it here for simplicity purposes
 //=================Require==================
 const mongoose = require('mongoose', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 //==============Setting connection==========
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose
-    .connect('mongodb://localhost:27017/farmStand')
-    .then(() => {
-      console.log('Mongo connection open...');
-    })
-    .catch((err) => {
-      console.log('Error. Unable to establish Mongo connection...');
-      console.log(err);
-    });
+	await mongoose
+		.connect('mongodb://localhost:27017/farmStand')
+		.then(() => {
+			console.log('Mongo connection open...');
+		})
+		.catch((err) => {
+			console.log('Error. Unable to establish Mongo connection...');
+			console.log(err);
+		});
 }
 //===============Mongoose final===================
 //==================================================
+
+app.use(express.urlencoded({extended: true}));
 
 //=============Views Engine==========
 app.set('view engine', 'ejs');
@@ -36,22 +38,35 @@ app.set('views', path.join(__dirname, '/views'));
 
 //=============Basic Route=============
 app.get('/products', async (req, res) => {
-  const products = await Product.find({}); //To Query every product
-  res.render('products/index', { products });
+	const products = await Product.find({}); //To Query every product
+	res.render('products/index', {products});
+});
+
+//======================================================
+//If a route goes after get by id route, it bteaks the code
+// Because interprets any additional url as an fake id
+//TODO: How to fix this behavior
+
+//==============Create a new product==============
+app.get('/products/new', (req, res) => {
+	res.render('products/new');
+});
+
+app.post('/products', async (req, res) => {
+	const newProduct = new Product(req.body);
+	await newProduct.save();
+	res.redirect(`/products/${newProduct._id}`);
 });
 
 //=============getById===========================
 app.get('/products/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render('products/show', { product });
+	const {id} = req.params;
+	const product = await Product.findById(id);
+	res.render('products/show', {product});
 });
+//========================================================
 
-//==============Create a new product==============
-app.get('/products/new', (req, res) => {
-  res.render('products/new');
-});
 //==============Port Listen==========
 app.listen(3000, () => {
-  console.log('Listening on the port: 3000...');
+	console.log('Listening on the port: 3000...');
 });
