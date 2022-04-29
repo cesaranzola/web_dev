0ca728172fe93c7f12a5d2f4e4d964b10b8be9d6
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-29 10:44:26 -0500

    Add solution for exercise 9

---
+++ Javascript_info/exercise9/app.js
@@ -0,0 +1,11 @@
+//=================Boolean Conversion =================
+//True
+console.log(Boolean(1)); // true
+console.log(Boolean('0')); // JavaScript treats any non-empty string as true
+console.log(Boolean(' ')); // JavaScript treats any non-empty string as true. Spaces are also not empty strings
+
+//False
+console.log(Boolean(0)); // False
+console.log(Boolean('')); // False > Empty String
+
+    
