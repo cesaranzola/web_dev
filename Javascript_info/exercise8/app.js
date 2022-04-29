b7c5515ffd5bdd00f47ce692a8c474d924735e1d
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-29 10:14:22 -0500

    Add solution for exercise 8

---
+++ Javascript_info/exercise8/app.js
@@ -0,0 +1,15 @@
+'use strict';
+//=================Type conversions =================
+
+//=================Numbers================
+console.log('16' / '2');
+
+let strToNum = '456';
+strToNum = Number(strToNum);
+console.log(strToNum);
+
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number('    123   '));
+console.log(Number('123z'));
+
