2c114ec17560d4e168efaf8f942be514f91fadc5
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-30 14:34:31 -0500

    Install express and basic dependencies

--- Javascript_info/exercise10/app.js
+++ Javascript_info/exercise10/app.js
@@ -29,3 +29,34 @@ console.log(apples + oranges);
 
 //Perform sum with the Unary Plus operator
 console.log(+apples + +oranges);
+
+//================Assignments =================
+let result = 34 + 2 + 3; // First evaluates then assigns to the variable result
+console.log(result);
+
+//================Assignment returns a value  =================
+// let a = 3;
+// let b = 2;
+
+// let c = 5 - (a = a + b);
+
+// console.log(c);
+
+//================Chaining =================
+let a, b, c;
+a = b = c = 2 + 2;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// ================= Modify in place =================
+let largeNUm = 4566;
+let newLargeNUm = (largeNUm += 343345453543);
+console.log(newLargeNUm);
+
+// Short 'modify and assign operators have the same precedence as the assignment operator
+let n = 2;
+n *= 2 + 4;
+console.log(n);
+
