6d2b5fbffe245811e182406262e73b43a394f58c
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-30 15:26:45 -0500

    Add basic data type examples & fix folder

---
+++ Javascript/exercise1/app.js
@@ -0,0 +1,22 @@
+//=================DATA TYPES=================
+// Define Variables
+let javascriptIsFun = true;
+console.log('javascriptIsFun');
+
+//Print to the console using typeof
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'username');
+
+//Changing a variable value
+javascriptIsFun = 'This is a severe affirmation';
+
+//Undefined values are both a value and a type
+let year;
+console.log(year);
+console.log(typeof year);
+
+//Null
+let nullValue = null;
+console.log(nullValue); // Returns object due to legacy reasons
