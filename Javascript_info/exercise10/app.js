a25330b3e6e3a9d884a8bdb13d1546a1b6d5e872
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-29 21:10:15 -0500

    Add other examples to exercise 10

--- Javascript_info/exercise10/app.js
+++ Javascript_info/exercise10/app.js
@@ -1,3 +1,4 @@
+'use strict';
 //================= Unary Plus operator doesn't affect numbers ================
 let x = 1;
 console.log(-x);
@@ -5,10 +6,26 @@ console.log(-x);
 let y = 1;
 console.log(+y);
 
-
 //===============Unary Plus operator affects non numbers ================
 let notNum = -'1';
 console.log(-notNum);
 
 let notNum2 = +'2';
 console.log(-notNum2);
+
+//==================Converts not numbers================
+let isGameOver = false;
+let youWin = true;
+
+console.log(+isGameOver);
+console.log(+youWin);
+
+//================Strings Examples =================
+let apples = '2';
+let oranges = '5';
+
+//Doesn't perform sum with Unary Plus operator
+console.log(apples + oranges);
+
+//Perform sum with the Unary Plus operator
+console.log(+apples + +oranges);
