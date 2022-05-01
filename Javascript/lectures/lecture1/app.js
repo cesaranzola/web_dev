6fd0973fb7ea618dc6423b21616641b9eace0c82
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-01 17:54:36 -0500

    Add lecture 1 examples

---
+++ Javascript/lectures/lecture1/app.js
@@ -0,0 +1,24 @@
+let js = 'amazing';
+console.log(js);
+
+console.log('John Doe');
+
+let firstName = 'John';
+console.log(firstName);
+
+
+//===============Variables naming conventions =============================
+//let 3year = 456; > invalid name > You can't start a variable with a number
+//Variables names can only contain letters, numbers, dollar signs and underscores
+//don't use reserve keyword to name variables
+//Don't call your variables name because it is a reserved word
+
+let $myMoney = 1000;
+let my_money = 145;
+
+//CamelCase it's consider the defacto standard for naming variables in JavaScript
+
+//Variables names should be descriptive
+let myFirstJob = 'Engineer';
+let mySecondJob = 'Lawyer';
+
