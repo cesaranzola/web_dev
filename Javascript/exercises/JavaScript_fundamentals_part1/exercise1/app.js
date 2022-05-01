5bee4a6406ff7e120a48dbb6ebb284c9203d4591
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-01 18:00:06 -0500

    Add solution for exercise 1

---
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise1/app.js
@@ -0,0 +1,6 @@
+const country = 'Island Red';
+const continent = 'Africa'
+let population = 1345;
+
+console.log(`The ${country} located in the ${continent} continent has a population of ${population}.`);
+
