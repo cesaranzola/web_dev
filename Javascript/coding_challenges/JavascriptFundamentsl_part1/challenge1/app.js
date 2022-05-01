fd2475a0b0758ea6f3d1412954001ca89f15b28a
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-30 22:59:03 -0500

    Test

--- Javascript/coding_challenges/JavascriptFundamentsl_part1/challenge1/app.js
+++ Javascript/coding_challenges/JavascriptFundamentsl_part1/challenge1/app.js
@@ -11,3 +11,5 @@ Test data:
 § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
 § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
 */
+
+const test = 34;
