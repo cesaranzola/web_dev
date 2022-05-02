2ea36b6cb28e28bac39369e607d1b3bd600aac91
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-02 15:05:44 -0500

    Add solution for coding challenge 2

---
+++ Javascript/coding_challenges/JavascriptFundamentsl_part1/challenge2/app.js
@@ -0,0 +1,27 @@
+let markHeight = 1.69;
+let markMass = 78;
+
+let johnHeight = 1.95;
+let johnMass = 92;
+
+//Calculate BMI
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+//Compare BMI
+let markHigherBMI = markBMI > johnBMI;
+
+// Results
+if (markHigherBMI > johnBMI) {
+	console.log(
+		`Mark's BMI of ${Math.floor(
+			markBMI
+		)} is higher than John's BMI ${Math.floor(johnBMI)}`
+	);
+} else {
+	console.log(
+		`John's BMI of ${Math.floor(
+			johnBMI
+		)} is higher than Mark's BMI of ${Math.floor(markBMI)}.`
+	);
+}
