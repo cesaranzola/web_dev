5c3338498a963b5d1ab2ecedd4c8908fc1706296
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-02 10:11:59 -0500

    Add solution for exercise 2

---
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise2/app.js
@@ -0,0 +1,6 @@
+const country = 'Germany'
+let population = 34000000;
+const isIsland = false;
+let language;
+
+console.log(`${country} has a population of ${population} and its language is ${language}. The affirmation that is an Island is ${isIsland}.`);
