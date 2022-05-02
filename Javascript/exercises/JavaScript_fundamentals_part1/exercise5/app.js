5b3e5e93791dc4873ddcc0e876e70c8b4546d102
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-02 14:23:04 -0500

    Add solution for exercise 5

---
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise5/app.js
@@ -0,0 +1,8 @@
+const country = 'Germany';
+let population = 36000000;
+const language = 'German';
+const continent = 'Europe';
+
+console.log(
+	`${country} has a population of ${population}, and it's located in ${continent}. It's official language is ${language}.`
+);
