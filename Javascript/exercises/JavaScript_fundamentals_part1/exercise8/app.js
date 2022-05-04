0040a8f9b2e4263f890f567660a92a98a5ad9e1f
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-03 20:23:52 -0500

    Create conditional statement

--- Javascript/exercises/JavaScript_fundamentals_part1/exercise8/app.js
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise8/app.js
@@ -2,4 +2,8 @@ const neighborsNum = prompt('How many neighbors countries does your country have
 
 if (neighborsNum == 1){
     console.log('Only one border')
+} else if (neighborsNum >= 2){
+    console.log('More than one border');
+} else {
+    console.log('No borders');
 }
