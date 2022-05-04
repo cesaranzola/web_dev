894d3a49657414cd7479f699eb600961b4b346da
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-03 21:03:41 -0500

    Add example with three logical operators

---
+++ Javascript/lectures/lecture11/app.js
@@ -0,0 +1,48 @@
+//=================Logical Operators =================
+// First test - Both values are true
+
+//============ and && operator =================
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
+
+// Second test - A is true and B is false
+// const hasDriversLicense = true; // A
+// const hasGoodVision = false; // B
+// console.log(hasDriversLicense && hasGoodVision);
+
+//============= or || operator =================
+// const hasDriversLicense = true; // A
+// const hasGoodVision = false; // B
+
+// console.log(hasDriversLicense || hasGoodVision);
+
+//============ not ! operator =================
+// const hasDriversLicense = true; // A
+// const hasGoodVision = false; // B
+// console.log(!hasDriversLicense);
+
+//================================================================
+// First example
+
+// const hasDriversLicense = true; // A
+// const hasGoodVision = false; // B
+// if (hasDriversLicense && hasGoodVision) {
+	// console.log('Sarah can drive.');
+// } else {
+	// console.log('Someone else should drive.');
+// }
+
+// Second example
+
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+const isTired = true; // C
+
+if (hasDriversLicense && hasGoodVision && isTired) {
+	console.log('Sarah can drive.');
+} else {
+	console.log('Someone else should drive.');
+}
+
