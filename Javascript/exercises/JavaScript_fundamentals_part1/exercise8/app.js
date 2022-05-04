7d2b28529d6478fa6794fcdacce43e0964c74fbc
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-03 20:30:43 -0500

    Add promt type number conversion to check for strict equality

--- Javascript/exercises/JavaScript_fundamentals_part1/exercise8/app.js
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise8/app.js
@@ -1,9 +1,9 @@
-const neighborsNum = prompt('How many neighbors countries does your country have?')
+const neighborsNum = Number(prompt('How many neighbors countries does your country have?'))
 
-if (neighborsNum == 1){
-    console.log('Only one border')
+if (neighborsNum === 1){
+    console.log('Only one border.')
 } else if (neighborsNum >= 2){
-    console.log('More than one border');
+    console.log('More than one border.');
 } else {
-    console.log('No borders');
+    console.log('No borders.');
 }
