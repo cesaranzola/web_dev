33ca94358c93499b7c12f85c7d93c7c637fb60c1
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-04 21:08:42 -0500

    Change name of varibles for readability

--- Javascript/coding_challenges/JavascriptFundamentsl_part1/challenge3/app.js
+++ Javascript/coding_challenges/JavascriptFundamentsl_part1/challenge3/app.js
@@ -1,31 +1,37 @@
 // Data set 1
-// const teamDolphins = (96 + 108 + 89) / 3;
-// const teamKoalas = (88 + 91 + 110) / 3;
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
 
 
 // Data set 2
-// const teamDolphins = (97 + 112 + 101) / 3;
-// const teamKoalas = (109 + 95 + 123) / 3;
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 123) / 3;
 
 // Data set 3
-const teamDolphins = (97 + 112 + 101) / 3;
-const teamKoalas = (109 + 95 + 106) / 3;
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 106) / 3;
 
 // Minimum score
 const minimumScore = 100;
 
 // Conditional statement
 
-if ((teamDolphins > teamKoalas) && (teamDolphins >= minimumScore)) {
+if ((scoreDolphins > scoreKoalas) && (scoreDolphins >= minimumScore)) {
 	console.log('Team Dolphins wins the tournament!');
-} else if ((teamKoalas > teamDolphins) && (teamKoalas >= minimumScore)) {
+} else if ((scoreKoalas > scoreDolphins) && (scoreKoalas >= minimumScore)) {
 	console.log('Team Koalas wins the tournament!');
 } else if (
-	(teamDolphins === teamKoalas) &&
-	(teamDolphins >= minimumScore) &&
-	(teamKoalas >= minimumScore)
+	(scoreDolphins === scoreKoalas) &&
+	(scoreDolphins >= minimumScore) &&
+	(scoreKoalas >= minimumScore)
 ) {
 	console.log(`It was a fierce battle Damen und Herren. It's a draw.`);
 } else {
 	console.log(`Neither team was able to reach the minimum score required to win.`);
 }
+
+
+// ==================================================
+// Notes on the video explanation 
+
+
