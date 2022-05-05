df583acc886b22a6dd5609a093de0e2c691caf05
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-04 20:16:15 -0500

    Add solution for exercise 9

---
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise9/app.js
@@ -0,0 +1,12 @@
+const country = 'Portugal';
+const population = 7000000;
+const language = 'Portuguese';
+const isIsland = false;
+
+if (population === 7000000 && language ===  'Portuguese' && !isIsland) {
+    console.log(`You should live in ${country}.`);
+} else {
+    console.log(`${country} doesn't meet your criteria.`);
+}
+
+
