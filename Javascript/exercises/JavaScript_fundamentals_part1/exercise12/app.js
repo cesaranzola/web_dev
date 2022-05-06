16257e37d434fd8a22d971a241dea9655cd619a6
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-05 21:39:42 -0500

    Add solution for the exercise 12 - Ternary operation

---
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise12/app.js
@@ -0,0 +1,9 @@
+// ================= Ternary operator =================
+const avgPopulation = 33000000;
+const portugalPopulation = 6000000;
+
+console.log(
+	`Portugal population is ${
+		portugalPopulation >= avgPopulation ? 'above' : 'below'
+	} average.`
+);
