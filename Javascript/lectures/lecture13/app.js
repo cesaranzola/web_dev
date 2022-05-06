b6ca2d0759ec614753e7a747647e2ee86d5993b5
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-05 20:51:46 -0500

    Add expressions and statements example

---
+++ Javascript/lectures/lecture13/app.js
@@ -0,0 +1,11 @@
+// Expressions vs conditionals
+
+// Expressions
+// An expression produces a value
+9 + 4;
+1345;
+true && true && false;
+
+/*
+Roughly, a statement performs an action. Loops and if statements are examples of statements. A program is basically a sequence of statements (we’re ignoring declarations here). Wherever JavaScript expects a statement, you can also write an expression. Such a statement is called an expression statement. The reverse does not hold: you cannot write a statement where JavaScript expects an expression. For example, an if statement cannot become the argument of a function.oughly, a statement performs an action. Loops and if statements are examples of statements. A program is basically a sequence of statements (we’re ignoring declarations here). Wherever JavaScript expects a statement, you can also write an expression. Such a statement is called an expression statement. The reverse does not hold: you cannot write a statement where JavaScript expects an expression. For example, an if statement cannot become the argument of a function.oughly, a statement performs an action. Loops and if statements are examples of statements. A program is basically a sequence of statements (we’re ignoring declarations here). Wherever JavaScript expects a statement, you can also write an expression. Such a statement is called an expression statement. The reverse does not hold: you cannot write a statement where JavaScript expects an expression. For example, an if statement cannot become the argument of a function.
+*/
