dd839a0ab5b807c223c45bf2f784e19d613e546b
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-05 22:50:51 -0500

    Add final solution for the coding challenge 4

--- Javascript/coding_challenges/JavascriptFundamentsl_part1/challenge4/app.js
+++ Javascript/coding_challenges/JavascriptFundamentsl_part1/challenge4/app.js
@@ -23,5 +23,33 @@
 // 	}`
 // );
 
-
 // Second solution
+// First data set
+// const bill = 275;
+// const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+
+// console.log(
+// 	`The bill was ${bill}, and tip was ${tip}, the total value was ${
+// 		bill + tip
+// 	}.`
+// );
+
+// Second data set
+// const bill = 40;
+// const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill
+
+// console.log(
+// 	`The bill was ${bill}, and tip was ${tip}, the total value was ${
+// 		bill + tip
+// 	}.`
+// );
+
+// Third data set
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+
+console.log(
+	`The bill was ${bill}, and tip was ${tip}, the total value was ${
+		bill + tip
+	}.`
+);
