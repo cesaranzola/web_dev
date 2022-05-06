679eb73a8fe2e1b6556b87d034d60f7f3ad6948c
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-05 21:30:14 -0500

    Add second switch example

--- Javascript/exercises/JavaScript_fundamentals_part1/exercise10/app.js
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise10/app.js
@@ -1,18 +1,24 @@
 // ================= Switch statement =================
 
-const language = 'Mandarin';
+const language = 'Kaparrapo';
 
 switch (language) {
-    case 'Mandarin':
-        console.log('It has the greatest number of native speakers');
-    case 'Spanish':
-        console.log('2nd place in the number of native speakers');
-    case 'English':
-        console.log('3rd place in the number of native speakers');
-    case 'Hindi':
-        console.log('4rd place in the number of native speakers');
-    case 'Arabic':
-        console.log('5th place in the number of native speakers');
-    default:
-        console.log('Great language too!');
+	case 'Mandarin':
+		console.log('It has the greatest number of native speakers.');
+		break;
+	case 'Spanish':
+		console.log('2nd place in the number of native speakers.');
+		break;
+	case 'English':
+		console.log('3rd place in the number of native speakers.');
+		break;
+	case 'Hindi':
+		console.log('4rd place in the number of native speakers.');
+		break;
+	case 'Arabic':
+		console.log('5th place in the number of native speakers.');
+		break;
+	default:
+		console.log('Great language too!');
 }
+
---
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise11/app.js
@@ -0,0 +1,28 @@
+const daysOfWeekGerman = 'Donnerstag';
+
+switch (daysOfWeekGerman) {
+	case 'Montag':
+		console.log('Montag is the first day of the week.');
+		break;
+	case 'Dienstag':
+		console.log('Dienstag is the second day of the week.');
+		break;
+	case 'Mittwoch':
+		console.log('Mittwoch is the third day of the week.');
+		break;
+	case 'Donnerstag':
+		console.log('Donnerstag is the fourth day of the week.');
+		break;
+	case 'Freitag':
+		console.log('Freitag is the fifth day of the week.');
+		break;
+	case 'Samstag':
+		console.log('Samstag is the sixth day of the week.');
+		break;
+	case 'Sonntag':
+		console.log('Sonntag is the seventh day of the week.');
+		break;
+	default:
+		console.log('Not a valid day of the week');
+}
+
