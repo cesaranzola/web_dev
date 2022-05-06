fbab4e1b59a69b1eca6c64465e2e167fb3b975b2
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-05 21:19:11 -0500

    Add the solution for the exercise 10 - Switch statement

---
+++ Javascript/exercises/JavaScript_fundamentals_part1/exercise10/app.js
@@ -0,0 +1,18 @@
+// ================= Switch statement =================
+
+const language = 'Mandarin';
+
+switch (language) {
+    case 'Mandarin':
+        console.log('It has the greatest number of native speakers');
+    case 'Spanish':
+        console.log('2nd place in the number of native speakers');
+    case 'English':
+        console.log('3rd place in the number of native speakers');
+    case 'Hindi':
+        console.log('4rd place in the number of native speakers');
+    case 'Arabic':
+        console.log('5th place in the number of native speakers');
+    default:
+        console.log('Great language too!');
+}
