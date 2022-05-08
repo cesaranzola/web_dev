90a7746cb32ac2ce41e877048ebea95260614c06
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-07 19:01:26 -0500

    Add addtional data for the functions exercise

--- Javascript/exercises/JavascriptFundamentsl_part2/exercise1/app.js
+++ Javascript/exercises/JavascriptFundamentsl_part2/exercise1/app.js
@@ -4,6 +4,10 @@ function describeCountry(country, population, capitalCity) {
 	return locationInfo;
 }
 
-const Heimat = describeCountry('Mexico', '50', 'Ciudad de Mexico - DF');
-console.log(Heimat);
+const home1 = describeCountry('Mexico', '50', 'Ciudad de Mexico - DF');
+const home2 = describeCountry('Argentina', '45', 'Buenos Aires');
+const home3 = describeCountry('France', '50', 'Paris');
 
+console.log(home1);
+console.log(home2);
+console.log(home3);
