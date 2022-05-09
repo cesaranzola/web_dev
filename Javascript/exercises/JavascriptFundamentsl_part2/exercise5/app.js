41ff8d3eea826cc3801a46b18d23aa4d05819edf
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-09 16:44:34 -0500

    Add solution for exercise 5

---
+++ Javascript/exercises/JavascriptFundamentsl_part2/exercise5/app.js
@@ -0,0 +1,39 @@
+// const worldPopulation = 7900;
+// const populations = [24, 50, 300, 45];
+// let percentages = [];
+
+// function percentageOfWorld(value) {
+// 	return ((value / worldPopulation) * 100).toFixed(2) + '%';
+// }
+
+// // Bad code example - waiting for loops lectures
+
+// const percentage1 = percentageOfWorld(populations[0]);
+// const percentage2 = percentageOfWorld(populations[1]);
+// const percentage3 = percentageOfWorld(populations[2]);
+// const percentage4 = percentageOfWorld(populations[3]);
+
+// percentages.push(percentage1);
+// percentages.push(percentage2);
+// percentages.push(percentage3);
+// percentages.push(percentage4);
+
+// console.log(percentages);
+
+// if (populations.length > 4) {
+// 	console.log(true);
+// } else {
+// 	console.log(false);
+// }
+
+//====================================================================================
+
+const worldPopulation = 7900;
+const populations = [24, 50, 300, 45];
+let percentages = [];
+
+for (let population of populations) {
+	percentages.push((population / worldPopulation) * 100).toFixed(2);
+}
+
+console.log(percentages);
