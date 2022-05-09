10fe20e42e715f6efcb65158ab38c48e42f6b553
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-08 19:50:19 -0500

    Refactor code for Fundamentals part1 - coding challenge 1

--- Javascript/coding_challenges/JavascriptFundamentsl_part2/challenge1/app.js
+++ Javascript/coding_challenges/JavascriptFundamentsl_part2/challenge1/app.js
@@ -1,29 +1,28 @@
 // ================= Coding challenge 1 =================
-const calcAverage = (score1, score2, score3) => {
-	return (score1 + score2 + score3) / 3;
-};
+const calcAvg = (a, b, c) => (a + b + c) / 3;
 
-function checkWinner(dolphinsAvg, koalasAvg) {
-	if (dolphinsAvg >= koalasAvg * 2) {
-		return `Dolphins wins ${dolphinsAvg} vs ${koalasAvg}`;
-	} else if (koalasAvg >= dolphinsAvg * 2) {
-		return `Koalas wins ${koalasAvg} vs ${dolphinsAvg}`;
+function checkWinner(teamName1, avg1, teamName2, avg2) {
+	if (avg1 >= avg2 * 2) {
+		// return `Dolphins wins (${avg1} vs ${avg2}).`;
+		console.log(`${teamName1} wins (${avg1} vs ${avg2}) to ${teamName2}.`);
+	} else if (avg2 >= avg1 * 2) {
+		// return `Koalas wins (${avg2} vs ${dolphinsAvg}).`;
+		console.log(
+			`${teamName2} wins (${avg2} vs ${avg1}) to the ${teamName1}.`
+		);
 	} else {
-		return `The game was suspended due to weather conditions.`;
+		// return `The game was suspended due to weather conditions.`;
+		console.log(`The game was suspended due to weather conditions.`);
 	}
 }
 
 // First data set
-// const dolphins = calcAverage(44, 23, 71);
-// const koalas = calcAverage(65, 54, 49);
-
-// const result = checkWinner(dolphins, koalas);
-// console.log(result);
+// checkWinner('Dolphins', calcAvg(85, 54, 41), 'Koalas', calcAvg(23, 34, 27));
 
 // Second data set
-const dolphins = calcAverage(85, 54, 41);
-const koalas = calcAverage(23, 34, 27);
-
-const result = checkWinner(dolphins, koalas);
-console.log(result);
-
+checkWinner(
+	'San francisco 49ers',
+	calcAvg(85, 54, 41),
+	'Los Angeles Rams',
+	calcAvg(23, 34, 27)
+);
