b65002d46777ae2c03b05b58808b39243634d159
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-11 23:16:58 -0500

    Lecture 8 - remove array methods

--- Javascript/lectures/lectues_fundamentals_part2/lecture8/app.js
+++ Javascript/lectures/lectues_fundamentals_part2/lecture8/app.js
@@ -1,5 +1,6 @@
 // ================= Array methods =================
 
+// ================= Add methods =================
 // Push - Adds an element to the end of the array
 const friends = ['Steve', 'John', 'Bob', 'Cooper'];
 friends.push('Layla');
@@ -17,3 +18,40 @@ console.log(newLength);
 // The unshift method also the length of the array
 friends.unshift('Robert');
 console.log(friends);
+
+// ================= Remove methods =================
+// Pop method - removes the last element of the array
+friends.pop(); // You don't need to pass any argument to the method
+console.log(friends);
+
+const popReturnValue = friends.pop();
+// the pop method returns the value that was removed
+console.log(popReturnValue);
+
+// Shift method - removes the first element of an array
+friends.shift();
+console.log(friends);
+
+// ================= IndexOf - to know the index of an element in an array ====================
+console.log(friends.indexOf('John'));
+
+// What would happen if you try to access the index of a non-existent element
+// It will return -1, signaling an error
+console.log(friends.indexOf('Fredo'));
+
+// ================= ES6 alternative to indexOf method  ==================
+// Include will return a boolean value, true if the element is present, false otherwise
+
+console.log(friends.includes('Cooper'));
+
+// This method uses strict equality
+friends.push('23');
+// It will return false, because it performs an strict equality check, and doesn't support coercions.
+console.log(friends.includes(23));
+
+// Use conditionals with includes method
+if (friends.includes('Cooper')) {
+	console.log('Black as a moonless night!');
+} else {
+	console.log('Without chemicals he points...');
+}
