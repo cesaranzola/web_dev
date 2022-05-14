4532a59fd75d128a9d60655486e64bd6d119eb2a
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-13 20:19:50 -0500

    Coding challenge 2 - Arrays and functions

--- Javascript/coding_challenges/JavascriptFundamentsl_part2/challenge2/app.js
+++ Javascript/coding_challenges/JavascriptFundamentsl_part2/challenge2/app.js
@@ -1,12 +1,28 @@
+const {json} = require('express/lib/response');
+
 // ================= Coding challenge 2 =================
 function calcTip(bill) {
 	if (bill >= 50 && bill <= 300) {
-		return `The bill was ${bill} and the tip was ${
-			bill * 0.15
-		}. The total is ${bill + bill * 0.15}.`;
+		return `The bill was $${bill} and $${bill * 0.15} for the tip. $${
+			bill + bill * 0.15
+		} in total.`;
 	} else {
-		return `The bill was ${bill} and the tip was ${
-			bill * 0.2
-		}. The total is ${bill + bill * 0.2}.`;
+		return `The bill was $${bill} and $${bill * 0.2} for the tip. $${
+			bill + bill * 0.2
+		} in total.`;
 	}
 }
+
+// Function test
+const result = calcTip(100);
+console.log(result);
+
+// Data set arrays
+
+let bills = [125, 555, 44];
+let tipsBills = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+// Total values
+console.log(tipsBills[0]);
+console.log(tipsBills[1]);
+console.log(tipsBills[2]);
