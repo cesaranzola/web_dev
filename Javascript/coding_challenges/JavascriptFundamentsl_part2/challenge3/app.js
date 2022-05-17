541698a29d65837492a860363ecd0cdace6cc506
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-16 20:28:43 -0500

    Add ternary operator & toFixed method to floor the BMI to two decimal points

--- Javascript/coding_challenges/JavascriptFundamentsl_part2/challenge3/app.js
+++ Javascript/coding_challenges/JavascriptFundamentsl_part2/challenge3/app.js
@@ -2,8 +2,8 @@ const markMiller = {
 	name: 'Mark Miller',
 	mass: 78,
 	height: 1.69,
-	calcBMI: function calcBMI(mass, height) {
-		return (this.mass * this.height) ** 2;
+	calcBMI: function calcBMI() {
+		return (this.mass / this.height ** 2).toFixed(2);
 	},
 };
 
@@ -11,7 +11,15 @@ const johnSmith = {
 	name: 'John Smith',
 	mass: 92,
 	height: 1.95,
-	calcBMI: function calcBMI(mass, height) {
-		return (this.mass * this.height) ** 2;
+	calcBMI: function calcBMI() {
+		return (this.mass / this.height ** 2).toFixed(2);
 	},
 };
+
+markMiller.calcBMI() > johnSmith.calcBMI()
+	? console.log(
+			`Marks's BMI ${markMiller.calcBMI()} is higher than  John's ${johnSmith.calcBMI()}`
+	)
+	: console.log(
+			`John's BMI ${johnSmith.calcBMI()} is higher than  Mark's ${markMiller.calcBMI()}`
+	);
