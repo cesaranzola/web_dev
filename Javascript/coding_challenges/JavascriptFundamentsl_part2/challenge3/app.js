c9cfcef94abe4708b53cca80a9e7401cedabda5e
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-16 20:05:18 -0500

    Add both objects and a prototype for the BMI method

---
+++ Javascript/coding_challenges/JavascriptFundamentsl_part2/challenge3/app.js
@@ -0,0 +1,17 @@
+const markMiller = {
+	name: 'Mark Miller',
+	mass: 78,
+	height: 1.69,
+	calcBMI: function calcBMI(mass, height) {
+		return (this.mass * this.height) ** 2;
+	},
+};
+
+const johnSmith = {
+	name: 'John Smith',
+	mass: 92,
+	height: 1.95,
+	calcBMI: function calcBMI(mass, height) {
+		return (this.mass * this.height) ** 2;
+	},
+};
