2fb80491714c7aba04c6f07680cea451338d093d
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-18 22:22:45 -0500

    Add bracket and dot notation example

--- Javascript/lectures/lectues_fundamentals_part2/lecture10/app.js
+++ Javascript/lectures/lectues_fundamentals_part2/lecture10/app.js
@@ -1,7 +1,20 @@
-const userName = {
-    firstName: 'John',
-    lastName: 'Wick',
-    username: 'gio4567',
-    password: 'route66high',
-    avatar: 'MonkeySuit'
-}
+const user = {
+	firstName: 'John',
+	lastName: 'Wick',
+	username: 'gio4567',
+	password: 'route66high',
+	avatar: 'MonkeySuit',
+};
+
+// How to retrieve data using the dot notation syntax
+console.log(user.firstName);
+
+// How to retrieve data using the bracket notation syntax
+console.log(user['lastName']);
+
+// How to write a computed property name using square bracket notation syntax
+const nameKey = 'Name';
+console.log(user['first' + nameKey]);
+console.log(user['last' + nameKey]);
+
+// This only works with the bracket notation syntax, with dot notation will throw an error.
