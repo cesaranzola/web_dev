006d98888d20c62ac42b7b6c86c0661f01f6f0d8
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-20 14:48:35 -0500

    Dot and bracket to assess an objects method

--- Javascript/lectures/lectues_fundamentals_part2/lecture11/app.js
+++ Javascript/lectures/lectues_fundamentals_part2/lecture11/app.js
@@ -5,8 +5,14 @@ const userData = {
 	job: 'Nurse',
 	friends: ['Michael', 'Peter', 'Steven'],
 	hasDriversLicense: true,
-    // Inside objects you need to write the functions as expressions, not as a declaration 
+	// Inside objects you need to write the functions as expressions, not as a declaration
 	calcAge: function (currentYear, yearOfBirth) {
-		return currentYear + yearOfBirth; 
+		return currentYear - yearOfBirth;
 	},
 };
+
+// Use a method from an object using the dot notation
+console.log(userData.calcAge(2054, 1968));
+
+// Use a method from and object using the bracket notation
+console.log(userData['calcAge'](2054, 1968));
