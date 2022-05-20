14c7bdcb6f4340078e0e4cefe75281168b569bb2
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-20 15:05:00 -0500

    Add a new property to the object

--- Javascript/lectures/lectues_fundamentals_part2/lecture11/app.js
+++ Javascript/lectures/lectues_fundamentals_part2/lecture11/app.js
@@ -6,13 +6,22 @@ const userData = {
 	friends: ['Michael', 'Peter', 'Steven'],
 	hasDriversLicense: true,
 	// Inside objects you need to write the functions as expressions, not as a declaration
-	calcAge: function (currentYear, yearOfBirth) {
-		return currentYear - yearOfBirth;
+	calcAge: function () {
+		// Remember the D.R.Y principle
+		this.age = 2050 - this.yearOfBirth;
+		return this.age; // It's always better to use the 'this' keyword and not hardcode the name of the object
 	},
 };
 
 // Use a method from an object using the dot notation
-console.log(userData.calcAge(2054, 1968));
+// console.log(userData.calcAge(2054));
 
-// Use a method from and object using the bracket notation
-console.log(userData['calcAge'](2054, 1968));
+// // Use a method from and object using the bracket notation
+// console.log(userData['calcAge'](2054));
+
+// Print the age using the age property from the userData object
+// You need to calculate the age only and once, and then use the age property to print the value
+console.log(userData.calcAge());
+console.log(userData.age);
+console.log(userData.age);
+console.log(userData.age);
