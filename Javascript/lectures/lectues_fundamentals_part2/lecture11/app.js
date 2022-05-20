6210045abdf65ae8a78e2f763492cb3931c7cfb7
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-20 15:43:28 -0500

    Add new method to object & use this keyword multiple times

--- Javascript/lectures/lectues_fundamentals_part2/lecture11/app.js
+++ Javascript/lectures/lectues_fundamentals_part2/lecture11/app.js
@@ -2,14 +2,24 @@ const userData = {
 	firstName: 'Linda',
 	lastName: 'Scott',
 	yearOfBirth: 1968,
-	job: 'Nurse',
+	job: 'nurse',
 	friends: ['Michael', 'Peter', 'Steven'],
-	hasDriversLicense: true,
+	hasDriversLicense: false,
 	// Inside objects you need to write the functions as expressions, not as a declaration
 	calcAge: function () {
 		// Remember the D.R.Y principle
 		this.age = 2050 - this.yearOfBirth;
 		return this.age; // It's always better to use the 'this' keyword and not hardcode the name of the object
+	},
+	summaryFunc: function () {
+		this.calcAge();
+		return (this.summary = `${this.firstName} ${this.lastName} is a ${
+			this.age
+		}-year old ${this.job}, and she ${
+			this.hasDriversLicense
+				? "has a driver's license."
+				: "doesn't have a driver's license."
+		}`);
 	},
 };
 
@@ -21,7 +31,8 @@ const userData = {
 
 // Print the age using the age property from the userData object
 // You need to calculate the age only and once, and then use the age property to print the value
-console.log(userData.calcAge());
-console.log(userData.age);
-console.log(userData.age);
-console.log(userData.age);
+// console.log(userData.calcAge());
+// console.log(userData.age);
+
+console.log(userData.summaryFunc());
+console.log(userData.summary);
