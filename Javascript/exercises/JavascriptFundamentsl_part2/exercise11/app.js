9ff3d8c478e5285bc4fb41602a3fa25ab3090af8
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-22 14:36:26 -0500

    Add break example

--- Javascript/exercises/JavascriptFundamentsl_part2/exercise11/app.js
+++ Javascript/exercises/JavascriptFundamentsl_part2/exercise11/app.js
@@ -7,8 +7,12 @@ const percentages = [];
 function percentageOfWorld() {
 	for (let i = 0; i < populations.length; i++) {
 		percentages.push(((populations[i] / worldPopulation) * 100).toFixed(2));
+		if (percentages <= 0.6) {
+			break;
+		}
 	}
 }
 
 percentageOfWorld();
+
 console.log(percentages);
