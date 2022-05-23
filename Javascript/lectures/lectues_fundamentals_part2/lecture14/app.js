ee292b66dbd6bb3f26c41df0afb2f9a413de4646
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-23 14:22:26 -0500

    Add nested loops example

--- Javascript/lectures/lectues_fundamentals_part2/lecture14/app.js
+++ Javascript/lectures/lectues_fundamentals_part2/lecture14/app.js
@@ -6,4 +6,16 @@ const user = [
 	['Michael', 'Jim', 'Steve'],
 ];
 
+// Backwards loop
 
+for (let i = user.length - 1; i >= 0; i--) {
+	console.log(user[i]);
+}
+
+// nested loops
+for (let exercise = 1; exercise <= 3; exercise++) {
+	console.log(`Starting exercise... ${exercise}`);
+	for (let i = 1; i <= 5; i++) {
+		console.log(`Exercise ${exercise}: Lifting weights... Rep ${i}`);
+	}
+}
