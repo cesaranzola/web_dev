86b1c80a5c32206211ba03b03f5281738dfc8297
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-23 14:53:52 -0500

    Add solution for exercise 12 - Nested loops

--- Javascript/exercises/JavascriptFundamentsl_part2/exercise12/app.js
+++ Javascript/exercises/JavascriptFundamentsl_part2/exercise12/app.js
@@ -3,3 +3,9 @@ const listOfNeighbors = [
 	['Spain'],
 	['Norway', 'Sweden', 'Russia'],
 ];
+
+for (let i = 0; i < listOfNeighbors.length; i++) {
+	for (let j = 0; j < listOfNeighbors[i].length; j++) {
+		console.log(` Neighbor: ${listOfNeighbors[i][j]}`);
+	}
+}
