2f5c46703d79f79c8dc26b93fa5ac873fe7e60e3
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-24 20:19:19 -0500

    Add final solution for exercise 9 - object with this keyword

--- Javascript/exercises/JavascriptFundamentsl_part2/exercise9/app.js
+++ Javascript/exercises/JavascriptFundamentsl_part2/exercise9/app.js
@@ -1,8 +1,19 @@
 const myCountry = {
 	country: 'United States',
 	population: '329.5 million',
+	language: 'English',
+	capital: 'Washington D.C',
 	neighboringCountries: ['Canada', 'Mexico', 'Cuba'],
 	checkIsIsland: function () {
-		return (this.isIsland = neighboringCountries > 0 ? true : false);
+		return (this.isIsland =
+			this.neighboringCountries.length > 0
+				? "It isn't an island"
+				: "It's an island.");
+	},
+	describeCountry: function () {
+		this.checkIsIsland();
+		return `${this.country} has a population of ${this.population}, and its capital is ${this.capital}. ${this.isIsland} and has ${this.neighboringCountries.length} neighbors. The official language is ${this.language}.`;
 	},
 };
+
+console.log(myCountry.describeCountry());
