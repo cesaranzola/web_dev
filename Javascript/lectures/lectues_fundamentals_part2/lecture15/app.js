07b7d1d0854371c5c72e416836997c5b224f348b
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-05-25 22:47:06 -0500

    Add while loop example

--- Javascript/lectures/lectues_fundamentals_part2/lecture15/app.js
+++ Javascript/lectures/lectues_fundamentals_part2/lecture15/app.js
@@ -2,3 +2,8 @@ for (let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights repetition ${rep}`);
 }
 
+let rep = 1;
+while (rep < 10) {
+    console.log(`Lifting weights repetition ${rep}`);
+    rep++;  
+}
