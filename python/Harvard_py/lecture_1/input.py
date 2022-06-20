a82f248584258e19b52007af82c883cfdf907c9e
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-20 16:30:39 -0500

    Add new separator for the print function

--- python/Harvard_py/lecture_1/input.py
+++ python/Harvard_py/lecture_1/input.py
@@ -6,7 +6,10 @@ name = input("What's your name? ")
 # print("Hello,", name)
 
 # How to modify the default behavior of the print function
-print("Hello, ", end="") # End modifies the default behavior of printing a new line, it does nothing instead
-print(name)
+# print("Hello, ", end="") # End modifies the default behavior of printing a new line, it does nothing instead
+# print(name)
 
+# Print sep parameter
+# It changes the single space separator for a double question mark
+print("Hello,", name, sep="??")
 
