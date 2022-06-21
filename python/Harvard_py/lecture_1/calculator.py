2297b297569d27620e7ea03753bec29d779d3262
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-21 14:25:23 -0500

    Add nested functions

--- python/Harvard_py/lecture_1/calculator.py
+++ python/Harvard_py/lecture_1/calculator.py
@@ -4,9 +4,18 @@
 # z = x + y
 # print(z)
 
-x = input("What's x? ")
-y = input("What's y? ")
 
-z = int(x) + int(y)
-print(z)
+# How to cast a value in python
+# x = input("What's x? ")
+# y = input("What's y? ")
+# 
+# z = int(x) + int(y)
+# print(z)
+
+# Nesting function - call function inside another function call
+# You can make an analogy with PEMDAS, and how numbers are evaluated
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+print(x + y)
 
