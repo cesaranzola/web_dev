8f0f64e33dc3c9021513c7a9a0b8b84b5621f64c
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-21 14:57:10 -0500

    Add default value for parameter

--- python/Harvard_py/lecture_1/hello_function.py
+++ python/Harvard_py/lecture_1/hello_function.py
@@ -1,6 +1,16 @@
-def hello():
-    print("Hello")
+# def hello(to):
+    # print("Hello,", to)
+# 
+# name = input("What's your name? ")
+# hello(name)
 
-name = input("What's your name? ")
+
+# Define default parameter in a function
+def hello(to):
+    print("Hello,", to="World")
+
 hello()
-print(name)
+name = input("What's your name? ")
+hello(name)
+
+
