bebdc6855309551677425f2dae4a81edf8a4d2c0
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-21 14:34:29 -0500

    Add a round number method

--- python/Harvard_py/lecture_1/calculator.py
+++ python/Harvard_py/lecture_1/calculator.py
@@ -14,8 +14,28 @@
 
 # Nesting function - call function inside another function call
 # You can make an analogy with PEMDAS, and how numbers are evaluated
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+# 
+# print(x + y)
 
-print(x + y)
+
+# Floating point calculator
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
+
+# z = x + y
+# print(z)
+
+# Round a number
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+z = round(x + y)
+print(z)
+
+
+
+
+
 
