a6488bf09e33776d7939961024c5970ddfee5a20
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-21 15:10:39 -0500

    Add a function call inside another function

--- python/Harvard_py/lecture_1/hello_function.py
+++ python/Harvard_py/lecture_1/hello_function.py
@@ -6,11 +6,28 @@
 
 
 # Define default parameter in a function
-def hello(to):
-    print("Hello,", to="World")
+# def hello(to="World"):
+    # print("Hello,", to)
+# 
+# hello()
+# name = input("What's your name? ")
+# hello(name)
 
-hello()
-name = input("What's your name? ")
-hello(name)
+# Calling functions inside another functions
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
+def hello(to="World"):
+    print("Hello,", to)
+
+main()
+
+
+
+
+
+
+
 
 
