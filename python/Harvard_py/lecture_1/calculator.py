43857a0ff9da77ed9990cce1440ef33790369e1b
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-21 14:50:46 -0500

    Final calculator example

--- python/Harvard_py/lecture_1/calculator.py
+++ python/Harvard_py/lecture_1/calculator.py
@@ -44,11 +44,20 @@
 
 
 # Round to an specific decimal points
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
+# 
+# z = round(x / y, 2)
+# print(z)
+
+# Alternative version 
 x = float(input("What's x? "))
 y = float(input("What's y? "))
 
-z = round(x / y, 2)
-print(z)
+z = x / y
+print(f"{z:.2f}")
+
+
 
 
 
