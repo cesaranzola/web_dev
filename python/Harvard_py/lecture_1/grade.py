e76cb163156dd797655bd9e1f50fa4b8ec012295
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-22 12:25:42 -0500

    Refactor the program with shorter syntax

--- python/Harvard_py/lecture_1/grade.py
+++ python/Harvard_py/lecture_1/grade.py
@@ -1,12 +1,26 @@
+# score = int(input("What's your score? "))
+# if score >= 90 and score <= 100:
+#     print("A")
+# elif score >=80 and score < 90:
+#     print("B")
+# elif score >= 70 and score < 60:
+#     print("C")
+# elif score >= 60 and score < 50:
+#     print("D")
+# else:
+#     print("F")
+    
 score = int(input("What's your score? "))
-if score >= 90 and score <= 100:
+if score >= 90:
     print("A")
-elif score >=80 and score < 90:
+elif score >=80:
     print("B")
-elif score >= 70 and score < 60:
+elif score >= 70:
     print("C")
-elif score >= 60 and score < 50:
+elif score >= 60:
     print("D")
 else:
     print("F")
+    
+    
     
