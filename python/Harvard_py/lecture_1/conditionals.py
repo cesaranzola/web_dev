64b08ca4e59e317579f64a5a30ba067c952cce07
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-22 11:59:20 -0500

    Add elif and else keywords to improve flow

--- python/Harvard_py/lecture_1/conditionals.py
+++ python/Harvard_py/lecture_1/conditionals.py
@@ -1,9 +1,22 @@
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+# 
+# if x < y:
+    # print("x is less than y")
+# if x > y:
+    # print("x is greater than y")
+# if x == y:
+    # print("x is equal to y")
+
+
+# Second iteration
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
 if x < y:
     print("x is less than y")
-if x > y:
+elif x > y:
     print("x is greater than y")
-if x == y:
+else:
     print("x is equal to y")
+    
