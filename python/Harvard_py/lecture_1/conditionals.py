bc4629ae07addeae461e4d529c30582b11221ee2
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-22 12:08:17 -0500

    Add not equal operator example

--- python/Harvard_py/lecture_1/conditionals.py
+++ python/Harvard_py/lecture_1/conditionals.py
@@ -23,10 +23,19 @@
     
     
 # Or operator 
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+# if x > y or x < y:
+    # print("x is not equal to y")
+# else:
+    # print("x is equal to y")
+
+# Not equal operator
 x = int(input("What's x? "))
 y = int(input("What's y? "))
-if x > y or x < y:
+if x != y:
     print("x is not equal to y")
 else:
     print("x is equal to y")
-
+    
+    
