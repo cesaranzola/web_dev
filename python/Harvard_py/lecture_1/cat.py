46a31406d442e9c5f5ac1ae4fd50a67cc64da9ab
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-23 11:33:34 -0500

    Add a for loop example

--- python/Harvard_py/lecture_1/cat.py
+++ python/Harvard_py/lecture_1/cat.py
@@ -3,8 +3,12 @@
 # print("meow")
 # print("meow")
 
-i = 3
-while i != 0:
-    print("meow")
-    i -= 1
+# i = 3
+# while i != 0:
+    # print("meow")
+    # i -= 1
 
+# For loop using lists
+# for i in [0, 1, 2]:
+    # print("meow")
+    
