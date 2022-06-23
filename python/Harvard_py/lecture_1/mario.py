aa2fe77a770e512aba35298ed389ec766fe6245a
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-23 13:50:17 -0500

    Add a function to print horizontally

--- python/Harvard_py/lecture_1/mario.py
+++ python/Harvard_py/lecture_1/mario.py
@@ -15,11 +15,20 @@
 # 
 # main()
 
-def main():
-    print_column(3)
+# def main():
+    # print_column(3)
+# 
+# def print_column(height):
+    # print("#\n" * height, end="")
+    # 
+# 
+# main()
 
-def print_column(height):
-    print("#\n" * height, end="")
-    
+def main():
+    print_row(4)
+
+
+def print_row(width):
+    print("?" * width)
 
 main()
