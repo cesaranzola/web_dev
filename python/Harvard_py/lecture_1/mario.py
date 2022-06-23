3a8df04fb79d7425e86c10c50db42d707065c3da
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-23 13:57:34 -0500

    Add a program to print a square with rows and columns

--- python/Harvard_py/lecture_1/mario.py
+++ python/Harvard_py/lecture_1/mario.py
@@ -24,11 +24,24 @@
 # 
 # main()
 
+# def main():
+#     print_row(4)
+
+
+# def print_row(width):
+#     print("?" * width)
+
+# main()
+
+
 def main():
-    print_row(4)
+    print_square(4)
 
-
-def print_row(width):
-    print("?" * width)
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")    
+        print("")
 
 main()
+    
