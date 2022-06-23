35ef276177b2564d040a8f16e1c15b9c50a8a918
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-23 12:02:06 -0500

    Add stepwise refinement functions for the meow program

--- python/Harvard_py/lecture_1/cat.py
+++ python/Harvard_py/lecture_1/cat.py
@@ -20,12 +20,29 @@
 # print("meow\n" * 3, end="")
 
 # While True example
-while True:
-    n = int(input("What's n? "))
-    if n > 0:
-        break
+# while True:
+#     n = int(input("What's n? "))
+#     if n > 0:
+#         break
 
-for _ in range(n):
-    print("meow")
+# for _ in range(n):
+#     print("meow")
     
+    
+def main():
+    n = get_num()
+    meow(n)
+    
+def get_num():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+        
+main()
     
