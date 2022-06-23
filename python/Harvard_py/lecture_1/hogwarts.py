9b75608ac905ce0f41640d177cfd9162b442d74e
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-23 12:11:39 -0500

    Add a list example & loop over it with a for loop

---
+++ python/Harvard_py/lecture_1/hogwarts.py
@@ -0,0 +1,10 @@
+# students = ["Hermione", "Harry", "Ron"]
+# print(students[0])
+# print(students[1])
+# print(students[2])
+
+# List using a for loop
+students = ["Hermione", "Harry", "Ron"]
+for student in students:
+    print(student)
+
