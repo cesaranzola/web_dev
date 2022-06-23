c001c752a455027841538eaffe57f7a5500260bb
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-23 13:19:25 -0500

    Add functions to replicate the mario program

--- python/Harvard_py/lecture_1/mario.py
+++ python/Harvard_py/lecture_1/mario.py
@@ -1,3 +1,25 @@
-print("#")
-print("#")
-print("#")
+# print("#")
+# print("#")
+# print("#")
+
+# for _ in range(3):
+    # print("#")
+
+
+# def main():
+    # print_column(3)
+# 
+# def print_column(height):
+    # for _ in range(height):
+        # print("#")
+# 
+# main()
+
+def main():
+    print_column(3)
+
+def print_column(height):
+    print("#\n" * height, end="")
+    
+
+main()
