adf2ae1b39b0b7cba6c2add7e547c1478ffdb75e
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-24 12:01:25 -0500

    Add a while True loop with try and except block

--- python/Harvard_py/lecture_3_4/number.py
+++ python/Harvard_py/lecture_3_4/number.py
@@ -5,10 +5,22 @@
 # except ValueError:
     # print("It is not an integer.")
 
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("It is not an integer.")
-else:
-    print(f"x is {x}")
+# try:
+    # x = int(input("What's x? "))
+# except ValueError:
+    # print("It is not an integer.")
+# else:
+    # print(f"x is {x}")
 
+# The else clause is associated with the try statement nor with the except clause.
+
+
+# Third iteration to the program
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("It is not an integer.")
+    else:
+        break
+print(f"x is {x}")
