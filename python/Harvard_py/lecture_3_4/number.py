391b1b438e37fef4c0838dcda04e75be8d4af6a3
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-24 12:08:40 -0500

    Add a function to handle the try and except clauses

--- python/Harvard_py/lecture_3_4/number.py
+++ python/Harvard_py/lecture_3_4/number.py
@@ -16,11 +16,30 @@
 
 
 # Third iteration to the program
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("It is not an integer.")
-    else:
-        break
-print(f"x is {x}")
+# while True:
+    # try:
+        # x = int(input("What's x? "))
+    # except ValueError:
+        # print("It is not an integer.")
+    # else:
+        # break
+# print(f"x is {x}")
+
+
+# Use functions with the try and except clauses
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x? "))
+        except ValueError:
+            print('It is not an integer.')
+        else:
+            break
+    return x
+
+main()
+
