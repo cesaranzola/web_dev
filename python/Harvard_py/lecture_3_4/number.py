9e5d81a253c46d164dbe212719bdfa8e81f16c1b
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-24 11:53:34 -0500

    Add else to the try and except example

--- python/Harvard_py/lecture_3_4/number.py
+++ python/Harvard_py/lecture_3_4/number.py
@@ -1,8 +1,14 @@
 
+# try:
+    # x = int(input("What's x? "))
+    # print(f"x is {x}")
+# except ValueError:
+    # print("It is not an integer.")
+
 try:
     x = int(input("What's x? "))
-    print(f"x is {x}")
 except ValueError:
     print("It is not an integer.")
-    
+else:
+    print(f"x is {x}")
 
