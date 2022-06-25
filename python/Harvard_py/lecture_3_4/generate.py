f2810bd2617f6ffc45d8fd438f13a7ede0483748
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-25 15:27:41 -0500

    Add a conditional statement to handle errors

--- python/Harvard_py/lecture_3_4/generate.py
+++ python/Harvard_py/lecture_3_4/generate.py
@@ -34,9 +34,18 @@
 # 
 # print("Hello, my name is " + str(sys.argv[1]))
 
-import sys
-try:
-    print("Hello, my name is " + str(sys.argv[1]))
-except IndexError:
-    print("Too few arguments.")
+# import sys
+# try:
+    # print("Hello, my name is " + str(sys.argv[1]))
+# except IndexError:
+    # print("Too few arguments.")
     
+import sys
+
+if len(sys.argv) < 2:
+    print("Too few arguments.")
+elif len(sys.argv) > 2:
+    print("Too many arguments.")
+else:
+    print("Hello, my name is " + str(sys.argv[1]))
+
