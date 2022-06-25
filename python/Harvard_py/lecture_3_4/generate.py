e59d3723b424c84f111b70f01ec0f3c77b8bf04a
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-25 15:35:45 -0500

    Refactor the conditional to separate errors from the print statement

--- python/Harvard_py/lecture_3_4/generate.py
+++ python/Harvard_py/lecture_3_4/generate.py
@@ -40,12 +40,23 @@
 # except IndexError:
     # print("Too few arguments.")
     
-import sys
+# import sys
+# 
+# if len(sys.argv) < 2:
+    # print("Too few arguments.")
+# elif len(sys.argv) > 2:
+    # print("Too many arguments.")
+# else:
+    # print("Hello, my name is " + str(sys.argv[1]))
 
+# You can pass the argv inside a string, so the program counts it as only one element
+
+import sys 
+# Check for errors
 if len(sys.argv) < 2:
     print("Too few arguments.")
 elif len(sys.argv) > 2:
     print("Too many arguments.")
-else:
-    print("Hello, my name is " + str(sys.argv[1]))
 
+# Print name tags
+print("Hello, my name is " + str(sys.argv[1]))
