b79e9f65eeacb544898eef7e90aaeedaad28c284
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-26 11:54:39 -0500

    Add a try and except with the assert keyword

--- python/Harvard_py/lecture_3_4/calculator.py
+++ python/Harvard_py/lecture_3_4/calculator.py
@@ -3,7 +3,7 @@ def main():
     square(x)
 
 def square(num):
-    return num * num
-
+    # return num * num
+    return num + num # deliver mistake to test a function 
 if __name__ == "__main__":
     main()
