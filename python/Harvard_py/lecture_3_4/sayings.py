d7401042431b87820c29903a9136e617e4e530cc
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-26 11:37:02 -0500

    Add comment to explain use of the __name__ convenction

--- python/Harvard_py/lecture_3_4/sayings.py
+++ python/Harvard_py/lecture_3_4/sayings.py
@@ -8,7 +8,25 @@ def hello(name):
 
 def goodbye(name):
     print(f"Goodbye, " + name)
-    
+
+
+# Only run thew function when yoy run the file directly, if you use it as a library it won't execute the main function     
+## double underscore functions belong to the program, and nobody is allowed to use it them as regular functions names
 if __name__ == "__main__":
     main()
+    
+# By default, Python names the programs as __main__, if you import a library the name that it will display is the name of the file not __main__
+
+
+
+
+
+
+
+
+
+
+
+
+
     
