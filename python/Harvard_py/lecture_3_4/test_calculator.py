6cef6d26d40d4dc7ca89ea3f37bd9c7a34f81a5b
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-26 12:05:31 -0500

    Add a simple test case to use with Pytest

--- python/Harvard_py/lecture_3_4/test_calculator.py
+++ python/Harvard_py/lecture_3_4/test_calculator.py
@@ -10,29 +10,37 @@ def main():
     # if square(3) != 9:
         # print("3 square was not 9.")
     
-# Example 2
-def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 squared was not 4.")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 squared was not 9.")
-    try:
-        assert square(-2) == 4
-    except AssertionError:
-        print("-2 squared was not 4.")
-    try:
-        assert square(-3) == 9
-    except AssertionError:
-        print("-3 squared was not 9.")
-    try:
-        assert square(0) == 0
-    except AssertionError:
-        print("0 squared was not 0.")
+# Example 2 - Using assert and try and except 
+# def test_square():
+#     try:
+#         assert square(2) == 4
+#     except AssertionError:
+#         print("2 squared was not 4.")
+#     try:
+#         assert square(3) == 9
+#     except AssertionError:
+#         print("3 squared was not 9.")
+#     try:
+#         assert square(-2) == 4
+#     except AssertionError:
+#         print("-2 squared was not 4.")
+#     try:
+#         assert square(-3) == 9
+#     except AssertionError:
+#         print("-3 squared was not 9.")
+#     try:
+#         assert square(0) == 0
+#     except AssertionError:
+#         print("0 squared was not 0.")
     
+    
+# Example 3
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
     
 if __name__ == "__main__":
     main()
