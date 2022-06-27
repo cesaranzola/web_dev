02ef4c8b748ee7d80fef3d04f950e9dc08c59df8
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-27 14:38:39 -0500

    Move dirs and files into proper location

--- python/Harvard_py/test_exercise/hello.py
+++ python/Harvard_py/test_exercise/hello.py
@@ -1,13 +1,9 @@
 def main():
-    name = int(input("What's your name?"))
-    print(hello(name))
-
+    hello("Cesar")
+    
 def hello(to="world"):
     return f"hello, {to}"
 
 if __name__ == "__main__":
     main()
 
-
-
-    
