9489f6dd843434dcd620629d9d901fccc99caa43
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-27 14:07:33 -0500

    Add a test for the hello function

--- python/Harvard_py/lecture_5_6/hello.py
+++ python/Harvard_py/lecture_5_6/hello.py
@@ -1,9 +1,12 @@
 def main():
     name = int(input("What's your name?"))
-    hello(name)
+    print(hello(name))
 
 def hello(to="world"):
-    print("hello,", to)
+    return f"hello,", {to}"
+
+if __name__ == "__main__":
+    main()
 
 
 
---
+++ python/Harvard_py/lecture_5_6/test_hello.py
@@ -0,0 +1,5 @@
+from hello import hello 
+
+def test_hello():
+    hello("Cesar") == "hello, Cesar"
+    
