f379cbb782812679cc25794335c971cdc5d11d04
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-28 15:00:16 -0500

    Add a new file, open it and save some input into it

--- python/Harvard_py/test_exercise/names.py
+++ python/Harvard_py/test_exercise/names.py
@@ -1,12 +1,18 @@
 # name = input("What's your name? ")
 # print(f"Hello, {name}")
 
-names = []
-for _ in range(3):
-    names.append(input("What's your name? "))
-
-for name in sorted(names):
-    print(f"Hello, {name}")
+# names = []
+# for _ in range(3):
+    # names.append(input("What's your name? "))
+# 
+# for name in sorted(names):
+    # print(f"Hello, {name}")
 
 
     
+name = input("What's your name? ")
+
+file = open("names.txt", 'w')
+file.write(name)
+file.close()
+
