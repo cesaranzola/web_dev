d201c5134067415682fcd1f5d7923a7b36a4885b
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-28 17:13:30 -0500

    Import csv library and use reader to loop and assign values to multiple variables

--- python/Harvard_py/test_exercise/students.py
+++ python/Harvard_py/test_exercise/students.py
@@ -75,15 +75,28 @@
     # print(f"{student['name']} is from  {student['home']}")
 
 
+# import csv
+# 
+# students = []
+# 
+# with open("students.csv") as file:
+    # reader = csv.reader(file)
+    # for row in reader:
+        # students.append({"name": row[0], "home": home})
+        # 
+# for student in sorted(students, key=lambda student: student["name"])
+    # print(f"{student['name']} is from {student['home']}")
+
+
 import csv
 
 students = []
 
 with open("students.csv") as file:
     reader = csv.reader(file)
-    for row in reader:
-        students.append({"name": row[0], "home": home})
+    for name, home in reader:
+        students.append({"name": name, "house": house})
         
-for student in sorted(students, key=lambda student: student["name"])
-    print(f"{student['name']} is from {student['home']}")
-
+for in student in sorted(students, key=lambda student: student["name"])
+    print(f"{student['name'], student['house']}")
+    
