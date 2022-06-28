ba1bc48f52688b982fce0de1d5186492d3c71e8c
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-28 14:56:13 -0500

    Add loops and sort user input

--- python/Harvard_py/test_exercise/names.py
+++ python/Harvard_py/test_exercise/names.py
@@ -1,2 +1,12 @@
-name = input("What's your name? ")
-print(f"Hello, {name}")
+# name = input("What's your name? ")
+# print(f"Hello, {name}")
+
+names = []
+for _ in range(3):
+    names.append(input("What's your name? "))
+
+for name in sorted(names):
+    print(f"Hello, {name}")
+
+
+    
