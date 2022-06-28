a5f30f7b15bae8ba07f066be6f9e5fa13686b296
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-28 17:23:44 -0500

    Add a DictReader

--- python/Harvard_py/test_exercise/students.py
+++ python/Harvard_py/test_exercise/students.py
@@ -88,15 +88,26 @@
     # print(f"{student['name']} is from {student['home']}")
 
 
-import csv
-
+# import csv
+# 
+# students = []
+# 
+# with open("students.csv") as file:
+    # reader = csv.reader(file)
+    # for name, home in reader:
+        # students.append({"name": name, "house": house})
+        # 
+# for in student in sorted(students, key=lambda student: student["name"])
+    # print(f"{student['name'], student['house']}")
+    
+# Dictionary reader 
+import csv 
 students = []
 
 with open("students.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "house": house})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
         
-for in student in sorted(students, key=lambda student: student["name"])
-    print(f"{student['name'], student['house']}")
-    
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['names']} is from {student['home']}")
