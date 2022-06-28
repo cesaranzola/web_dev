2be8b58d855e678386382b68aa3bc66f229f05c0
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-28 16:10:11 -0500

    Add a lamdda function to handle the sort

--- python/Harvard_py/test_exercise/students.py
+++ python/Harvard_py/test_exercise/students.py
@@ -8,12 +8,55 @@
         # name, house = line.rstrip().split(",")
         # print(f"{name} is in {house}")
 
+# students = []
+# with open("students.csv") as file:
+    # for line in file:
+        # name, house = line.rstrip().split(",")
+        # students.append(f"{name} is in {house}")
+        # 
+# for student in sorted(students):
+    # print(student)
+    
+    
+    
+# students = []
+# with open("students.csv") as file:
+    # for line in file:
+        # name, house = line.rstrip().split(",")
+        # student = {}
+        # student = ["name"] = name
+        # student = ["houses"] = house
+        # students.append(student)
+        # 
+# for student in students:
+    # print(f"{student['name']} is in {student['house']}")
+    
+
+# students = []
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         student = {"name": name, "house": house}
+#         students.append(student)
+
+# def get_name(student):
+#     return student["name"]
+
+# for student in sorted(students, key="get_name")j
+#     print(f"{student['name']} is in {student['house']}")
+    
+    
+    
+    
+
 students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        students.append(f"{name} is in {house}")
-        
-for student in students:
-    print(student)
-    
+        student = {"name": name, "house": house}
+        students.append(student)
+
+
+# When yoy have a anonymous function you use the lambda keyword
+for student in sorted(students, lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
