7bbd9df99adf286c3c8652ff8257883420b6833a
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-28 15:46:19 -0500

    Append data to empty list & sort it

--- python/Harvard_py/test_exercise/students.py
+++ python/Harvard_py/test_exercise/students.py
@@ -1,5 +1,19 @@
+# with open("students.csv") as file:
+    # for line in file:
+        # row = line.rstrip().split(",")
+        # print(f"{row[0]} is in {row[1]}")
+
+# with open("students.csv") as file:
+    # for line in file:
+        # name, house = line.rstrip().split(",")
+        # print(f"{name} is in {house}")
+
+students = []
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        students.append(f"{name} is in {house}")
+        
+for student in students:
+    print(student)
     
