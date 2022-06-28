2c06ce0a2c0b53bb37323899f4c25ab128ca07dc
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-28 15:06:28 -0500

    Add a new line each time you append data to the file

--- python/Harvard_py/test_exercise/names.py
+++ python/Harvard_py/test_exercise/names.py
@@ -9,10 +9,16 @@
     # print(f"Hello, {name}")
 
 
-    
+
+# name = input("What's your name? ")
+
+# "w" overwrites each time the file, if yoy want to add subsequent data, you have to change it to 'a' that stands for append
+# file = open("names.txt", 'w')
+# file.write(name)
+# file.close()
+
+
 name = input("What's your name? ")
-
-file = open("names.txt", 'w')
-file.write(name)
+file = open("names.txt", 'a')
+file.write(f"{name}\n")
 file.close()
-
