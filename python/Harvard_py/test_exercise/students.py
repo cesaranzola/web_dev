b19ea34bb73a2240fe8b03e9cc80df9a73a8eb89
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-28 17:41:30 -0500

    Write a file using DictWriter and writerrow method

--- python/Harvard_py/test_exercise/students.py
+++ python/Harvard_py/test_exercise/students.py
@@ -101,13 +101,22 @@
     # print(f"{student['name'], student['house']}")
     
 # Dictionary reader 
-import csv 
-students = []
+# import csv 
+# students = []
+# 
+# with open("students.csv") as file:
+    # reader = csv.DictReader(file)
+    # for row in reader:
+        # students.append({"name": row["name"], "home": row["home"]})
+        # 
+# for student in sorted(students, key=lambda student: student["name"]):
+    # print(f"{student['name']} is from {student['home']}")
+
+import csv
+
+name = input("What's your name? ")
+home = input("What's your home? ")
 
 with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row["name"], "home": row["home"]})
-        
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
