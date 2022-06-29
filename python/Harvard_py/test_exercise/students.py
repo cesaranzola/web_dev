a2c5b12be2abec784fed476951d766501f25615e
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-29 11:19:19 -0500

    Add gif program

--- python/Harvard_py/test_exercise/students.py
+++ python/Harvard_py/test_exercise/students.py
@@ -117,6 +117,6 @@ import csv
 name = input("What's your name? ")
 home = input("What's your home? ")
 
-with open("students.csv") as file:
+with open("students.csv", "a") as file:
     writer = csv.DictWriter(file, fieldnames=["name", "home"])
     writer.writerow({"name": name, "home": home})
