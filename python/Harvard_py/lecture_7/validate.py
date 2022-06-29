7110cf265d93de5c74495db9fb0bc11ce89b067d
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-29 12:23:01 -0500

    Add a program using RegeX functions to print if a email is valid or not

--- python/Harvard_py/lecture_7/validate.py
+++ python/Harvard_py/lecture_7/validate.py
@@ -1,6 +1,55 @@
-email = input("What's your email? ").strip()
+# email = input("What's your email? ").strip()
+# 
+# if "@" in email:
+    # print("Valid")
+# else:
+    # print("Invalid")
 
-if "@" in email:
+# email = input("Please enter your email address: ").strip()
+# 
+# if "@" in email and "." in email:
+    # print("Valid")
+# else:
+    # print("Invalid")
+
+# email = input("Please enter your email address: ").strip()
+# username, domain = email.split("@")
+# 
+# if username  and "." in domain:
+    # print("Valid")
+# else:
+    # print("Invalid")
+
+# email = input("Please enter your email address: ").strip()
+# username, domain = email.split("@")
+# 
+# if username  and domain.endswith(".edu"):
+    # print("Valid")
+# else:
+    # print("Invalid")
+    
+    
+# import re
+# email = input("Please enter your email address: ").strip()
+# 
+# if re.search(".+@.+\.edu", email):
+    # print("Valid")
+# else:
+    # print("Invalid")
+
+
+# import re
+# email = input("Please enter your email address: ").strip()
+# 
+# if re.search("^.+@.+\.edu$", email):
+    # print("Valid")
+# else:
+    # print("Invalid")
+    
+import re
+email = input("Please enter your email address: ").strip()
+
+if re.search("^[^]+@[^@]+\.edu$", email):
     print("Valid")
 else:
     print("Invalid")
