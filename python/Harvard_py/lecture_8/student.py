654acfdcfa5036e5b7cb0545839d6b7f8745bb58
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-06-30 20:59:17 -0500

    Add functions to take input & only call main if it is other file

--- python/Harvard_py/lecture_8/student.py
+++ python/Harvard_py/lecture_8/student.py
@@ -1,3 +1,14 @@
-name = input("What's your name? ")
-house = input("What's your house? ")
-print(f"{name} {house}")
+def main():
+    name = get_name()
+    house = get_house()
+    print(f"{name} from {house}")
+
+def get_name():
+    return input("Name: ")
+
+def get_house():
+    return input("House: ")
+
+if __name__ == "__main__":
+    main()
+
