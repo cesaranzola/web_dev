cb275a06903a82d495a2292673e9b40fb0088cf3
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-01 15:20:41 -0500

    Add input as a dict

--- python/Harvard_py/lecture_8/student.py
+++ python/Harvard_py/lecture_8/student.py
@@ -1,12 +1,49 @@
+#=========================
+# immutability
+
+# def main():
+    # student = get_student()
+    # print(f"{student[0]} from {student[1]}")
+# 
+# def get_student():
+    # name = input("Name: ")
+    # house = input("House: ")
+    # return (name, house)
+# 
+# if __name__ == "__main__":
+    # main()
+
+# Mutable list 
+# def main():
+    # student = get_student()
+    # if student[0] == "Padma":
+        # student[1] == "Ravenclaw"
+    # print(f"{student[0]} from {student[1]}")
+# 
+# def get_student():
+    # name = input("Name: ")
+    # house = input("House: ")
+    # return [name, house]
+# 
+# if __name__ == "__main__":
+    # main()
+    
+    
+# Dictionary
+
+
 def main():
     student = get_student()
-    print(f"{student[0]} from {student[1]}")
-
+    print(f"{student['name']} from {student['house']}")
+    
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return (name, house)
+    student = {}
+    student['name'] = input("Name: ")
+    student['house'] = input("House: ")
+    return student
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
+    
+    
+    
