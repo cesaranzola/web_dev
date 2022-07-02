35d67370e901dad9c1a05f49fea4db85882d5d91
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-02 13:38:31 -0500

    Create a new class

--- python/Harvard_py/lecture_8/student.py
+++ python/Harvard_py/lecture_8/student.py
@@ -30,20 +30,22 @@
     
     
 # Dictionary
-
+class Student():
+# class Student:
+    
+    
 
 def main():
     student = get_student()
     print(f"{student['name']} from {student['house']}")
     
 def get_student():
-    student = {}
-    student['name'] = input("Name: ")
-    student['house'] = input("House: ")
-    return student
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student 
 
 if __name__ == '__main__':
     main()
-    
     
     
