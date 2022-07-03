74cd004145002e56b253587353e14618ac712f09
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-03 15:35:11 -0500

    Add conditionals and raise statements to the Student class

--- python/Harvard_py/lecture_8/student.py
+++ python/Harvard_py/lecture_8/student.py
@@ -30,12 +30,17 @@
     
     
 # Dictionary
-from webbrowser import get
+
 
 
 class Student():
     # Dunder or magic methods in python
     def __init__(self, name, house):
+        # This type of conditional assignments are unique to classes, a dict will add a key automatically whenever you assign  one to it
+        if not name:
+            raise ValueError("Missing name")
+        if house != ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 # class Student:
@@ -46,6 +51,7 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
+    # Constructor form - It's going to instantiate a Student object for you, by using the student class as a mold or blueprint
     student = Student(name, house)
     return student 
 
