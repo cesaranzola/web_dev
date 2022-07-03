0e4f999c8bd5e756845f729e5721819a6183551b
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-03 15:17:55 -0500

    Add a Dunder method and class

--- python/Harvard_py/lecture_8/student.py
+++ python/Harvard_py/lecture_8/student.py
@@ -30,19 +30,23 @@
     
     
 # Dictionary
-class Student():
-# class Student:
-    
-    
+from webbrowser import get
 
+
+class Student():
+    # Dunder or magic methods in python
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+# class Student:
 def main():
     student = get_student()
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
     
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
     return student 
 
 if __name__ == '__main__':
