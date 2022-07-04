ec95d7b0a6864bb0da271512d9ef535cb9b1370b
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-04 12:42:32 -0500

    Add a str class special method to change the return value

--- python/Harvard_py/lecture_8/student.py
+++ python/Harvard_py/lecture_8/student.py
@@ -39,14 +39,17 @@ class Student():
         # This type of conditional assignments are unique to classes, a dict will add a key automatically whenever you assign  one to it
         if not name:
             raise ValueError("Missing name")
-        if house != ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        
+    def __str__(self):
+        return "A student"
 # class Student:
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
     
 def get_student():
     name = input("Name: ")
