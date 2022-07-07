c5e1a67d961a37cc768ee87a4d70d2d61df77e9d
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-07 12:00:10 -0500

    Add a class vault

---
+++ python/Harvard_py/lecture_8/vault.py
@@ -0,0 +1,8 @@
+class Vault:
+    def __init__(self, galleons=0, sickles=0, knuts=0):
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
+
+potter = Vault(100, 50, 25)
+print(potter)
--- python/Harvard_py/lecture_8/wizard.py
+++ python/Harvard_py/lecture_8/wizard.py
@@ -4,12 +4,12 @@ class Wizard:
             raise ValueError("Missing name")
         self.name = name
 
-class Student:
+class Student(Wizard):
     def __init__(self, name, house):
         super().__init__(name)
         self.house = house
         
-class Professor:
+class Professor(Wizard):
     def __init__(self, name, subject):
         super().__init__(name)
         self.subject = subject
