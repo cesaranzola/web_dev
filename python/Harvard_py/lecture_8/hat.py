3ff622b3a51795f7f5ab26605dffd588fcc2db44
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-07 11:21:20 -0500

    Add a classmethod and class variable to handle the sort of a house

--- python/Harvard_py/lecture_8/hat.py
+++ python/Harvard_py/lecture_8/hat.py
@@ -1,11 +1,11 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-        
-    def sort(self, name):
-        house = random.choice(self.houses)
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    @classmethod    
+    def sort(cls, name):
+        house = random.choice(cls.houses)
         print(name, "is in", house)
 
 hat = Hat()
