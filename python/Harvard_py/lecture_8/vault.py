4444784d558ba8a2154fc3cf6c2a669a8ae5fa58
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-07 12:11:59 -0500

    Add multiple vaults and use the overloading for + operator

--- python/Harvard_py/lecture_8/vault.py
+++ python/Harvard_py/lecture_8/vault.py
@@ -3,6 +3,19 @@ class Vault:
         self.galleons = galleons
         self.sickles = sickles
         self.knuts = knuts
+    
+    def __str__(self):
+        return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} knuts"
 
 potter = Vault(100, 50, 25)
 print(potter)
+
+weasley = Vault(25, 20, 100)
+print(weasley)
+
+galleons = potter.galleons + weasley.galleons
+sickles = potter.sickles + weasley.sickles
+knuts = potter.knuts + weasley.knuts
+
+total = Vault(galleons, sickles, knuts)
+print(total)
