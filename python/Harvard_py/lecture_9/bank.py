c9412307999e15c75df415b043a47f02ee46091c
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-09 13:19:16 -0500

    Add an account class and use OOP to manage balance

--- python/Harvard_py/lecture_9/bank.py
+++ python/Harvard_py/lecture_9/bank.py
@@ -1,18 +1,23 @@
-balance = 0
-
+class Account:
+    def __init__(self):
+        self._balance = 0
+    
+    @property
+    def balance(self):
+        return self._balance
+    
+    def deposit(self, amount):
+        self._balance += amount
+        
+    def withdraw(self, amount):
+        self._balance -= amount
+    
 def main():
-    print("Balance:", balance)
-    deposit(100)
-    withdraw(50)
-    print("Balance:", balance)
-    
-def deposit(amount):
-    global balance
-    balance += amount
-    
-def withdraw(amount):
-    global balance
-    balance -= amount
-    
+    account = Account()
+    print("Balance:", account.balance)
+    account.deposit(100)
+    account.withdraw(78)
+    print("Balance:", account.balance)
+
 if __name__ == "__main__":
     main()
