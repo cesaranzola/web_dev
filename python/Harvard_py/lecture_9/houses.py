ace8336ee917a518a7cf913911f2cd03a7c6fd89
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-09 12:52:45 -0500

    Add a bank program

--- python/Harvard_py/lecture_9/houses.py
+++ python/Harvard_py/lecture_9/houses.py
@@ -1,7 +1,18 @@
 students = [
-    {"name": "Hermione", "house": "Gryffindor",}
-    {"name": "Harry", "house": "Gryffindor",}
-    {"name": "Ron", "house": "Gryffindor",}
-    {"name": "Draco", "house": "Slytherin",}
-    {"name": "Padma", "house": "Ravenclaw",}    
+    {"name": "Hermione", "house": "Gryffindor",},
+    {"name": "Harry", "house": "Gryffindor",},
+    {"name": "Ron", "house": "Gryffindor",},
+    {"name": "Draco", "house": "Slytherin",},
+    {"name": "Padma", "house": "Ravenclaw",},   
 ]
+
+houses = set()
+
+for student in students:
+    houses.add(student["house"])
+        
+for house in sorted(houses):
+    print(house)
+    
+
+    
