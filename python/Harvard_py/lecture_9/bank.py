ffe82739fc0050802a2cf715a4ebc8ffaffbbd3d
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-09 12:57:37 -0500

    Add global keyword to variable

--- python/Harvard_py/lecture_9/bank.py
+++ python/Harvard_py/lecture_9/bank.py
@@ -2,6 +2,17 @@ balance = 0
 
 def main():
     print("Balance:", balance)
+    deposit(100)
+    withdraw(50)
+    print("Balance:", balance)
+    
+def deposit(amount):
+    global balance
+    balance += amount
+    
+def withdraw(amount):
+    global balance
+    balance -= amount
     
 if __name__ == "__main__":
     main()
