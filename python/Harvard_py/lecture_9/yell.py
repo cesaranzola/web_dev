413a9958c957400986597af16d770b96ed990dad
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-11 15:07:23 -0500

    Spread the data from a variadic function args

--- python/Harvard_py/lecture_9/yell.py
+++ python/Harvard_py/lecture_9/yell.py
@@ -1,8 +1,11 @@
 def main():
-    yell("This is CS50")
+    yell("This",  "is", "CS50")
     
-def yell(sentence):
-    print(sentence.upper())
-
+def yell(*words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+    
 if __name__ == '__main__':
     main()
