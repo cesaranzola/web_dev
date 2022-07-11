4d4ee4d7805bc98a4c081368506ea9f5db9950d9
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-07-11 16:03:19 -0500

    Use yield keyword to generate data in chuncks and avoid ran of out memory

--- python/Harvard_py/lecture_9/sleep.py
+++ python/Harvard_py/lecture_9/sleep.py
@@ -1,8 +1,13 @@
 def main():
     n = int(input("What's n? "))
+    for s in sheep(n):
+        print(s)
+        
+def sheep(n):
+    flock = []
     for i in range(n):
-        print("🐑" * i)
-
+        yield "🐑" * i
+        
 if __name__ == "__main__":
     main()
     
